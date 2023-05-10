93a0d3353d2afedf8cc5854ab48c5fe9139db940
Ritick Madaan <ritickmadaan57@gmail.com>
2023-05-10 19:57:49 +0530

    flakes first cut

---
+++ backend/flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+
+      in rec {
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage {
+          nativeBuildInputs = with pkgs; [ postgresql_12 ];
+          src = ./.;
+        };
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs;
+          [
+            rustc
+            cargo
+            postgresql_12
+            libiconv
+            openssl
+          ];
+          # buildInputs = with pkgs; [ ];
+        };
+      }
+    );
+}
--- backend/src/db/schema.rs
+++ backend/src/db/schema.rs
@@ -1,6 +1,24 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    contexts (key) {
+        key -> Varchar,
+        value -> Varchar,
+        last_modified -> Timestamptz,
+        created_on -> Timestamptz,
+    }
+}
+
+diesel::table! {
+    ctxoverrides (context_id) {
+        context_id -> Varchar,
+        override_id -> Varchar,
+        last_modified -> Timestamptz,
+        created_on -> Timestamptz,
+    }
+}
+
+diesel::table! {
     dimensions (dimension) {
         dimension -> Varchar,
         priority -> Int4,
@@ -28,15 +46,6 @@ diesel::table! {
 }
 
 diesel::table! {
-    contexts (key) {
-        key -> Varchar,
-        value -> Varchar,
-        last_modified -> Timestamptz,
-        created_on -> Timestamptz,
-    }
-}
-
-diesel::table! {
     newcontexts (key) {
         key -> Varchar,
         value -> Json,
@@ -49,20 +58,11 @@ diesel::table! {
     }
 }
 
-diesel::table! {
-    ctxoverrides (context_id) {
-        context_id -> Varchar,
-        override_id -> Varchar,
-        last_modified -> Timestamptz,
-        created_on -> Timestamptz,
-    }
-}
-
 diesel::allow_tables_to_appear_in_same_query!(
     contexts,
     ctxoverrides,
     dimensions,
     global_config,
     overrides,
-    newcontexts
+    newcontexts,
 );
