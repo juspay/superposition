a89b48599d0eb63f9c89c664987d96f77feabfae
Shrey Bana <shrey.bana@juspay.in>
2023-05-30 15:44:58 +0530

    feat:  Added context list API
    - Exposed GET@/context/list?page=<number>&size=<number>

--- src/v1/api/context/handlers.rs
+++ src/v1/api/context/handlers.rs
@@ -1,11 +1,14 @@
 use crate::{
     db::utils::AppState,
     v1::{
-        api::context::types::{AddContextReq, AddContextResp},
+        api::context::types::{AddContextReq, AddContextResp, PaginationParams},
         db::{
-            models::{Context, Override, Dimension},
-            schema::{contexts::dsl::contexts, overrides::dsl::overrides, dimensions::dsl::dimensions},
+            models::{Context, Dimension, Override},
+            schema::{
+                contexts::dsl::contexts, dimensions::dsl::dimensions, overrides::dsl::overrides,
+            },
         },
+        helpers::ToActixErr,
     },
 };
 use actix_web::{
@@ -19,43 +22,40 @@ use diesel::{
     result::{DatabaseErrorKind::*, Error::DatabaseError},
     ExpressionMethods, PgConnection, QueryDsl, QueryResult, RunQueryDsl,
 };
-use serde_json::{json, Value};
+use serde_json::{json, Value, Value::Null};
 
 pub fn endpoints() -> Scope {
     Scope::new("")
         .service(add_contexts_overrides)
         .service(get_context)
+        .service(list_contexts)
 }
 
 type DBConnection = PooledConnection<ConnectionManager<PgConnection>>;
 
-fn val_dimensions_cal_priority(
-    conn: &mut DBConnection,
-    cond: &Value,
-) -> Result<i32, String> {
+fn val_dimensions_cal_priority(conn: &mut DBConnection, cond: &Value) -> Result<i32, String> {
     let mut get_priority = |key: &String, val: &Value| -> Result<i32, String> {
         if key == "var" {
             let dimension_name = val
                 .as_str()
                 .ok_or_else(|| "Dimension name should be of String type")?;
-            dimensions.find(dimension_name)
+            dimensions
+                .find(dimension_name)
                 .first(conn)
                 .map(|d: Dimension| d.priority)
-                .map_err(|e| format!("{dimension_name}: {}",e.to_string()))
+                .map_err(|e| format!("{dimension_name}: {}", e.to_string()))
         } else {
             val_dimensions_cal_priority(conn, val)
         }
     };
 
     match cond {
-        Value::Object(x) => 
-            x.iter().try_fold(0, |acc, (key, val)| 
-                get_priority(key, val).map(|res| res + acc)
-            ),
-        Value::Array(x) => 
-            x.iter().try_fold(0, |acc, item|
-                val_dimensions_cal_priority(conn, item).map(|res| res + acc)
-            ),
+        Value::Object(x) => x.iter().try_fold(0, |acc, (key, val)| {
+            get_priority(key, val).map(|res| res + acc)
+        }),
+        Value::Array(x) => x.iter().try_fold(0, |acc, item| {
+            val_dimensions_cal_priority(conn, item).map(|res| res + acc)
+        }),
         _ => Ok(0),
     }
 }
@@ -78,8 +78,12 @@ async fn add_contexts_overrides(
     };
 
     let priority = match val_dimensions_cal_priority(&mut conn, &ctxt_cond) {
-        Ok(0) => { return HttpResponse::BadRequest().body("No dimension found in contexts"); },
-        Err(e) => { return HttpResponse::BadRequest().body(e); }
+        Ok(0) => {
+            return HttpResponse::BadRequest().body("No dimension found in contexts");
+        }
+        Err(e) => {
+            return HttpResponse::BadRequest().body(e);
+        }
         Ok(p) => p,
     };
     let context_id = blake3::hash((ctxt_cond).to_string().as_bytes()).to_string();
@@ -158,4 +162,40 @@ async fn get_context(path: web::Path<String>, state: Data<AppState>) -> Result<i
         Some(ctx) => Ok(web::Json(ctx.clone())),
         _ => Err(error::ErrorNotFound("")),
     }
+}
+
+#[get("/list")]
+async fn list_contexts(
+    qparams: web::Query<PaginationParams>,
+    state: Data<AppState>,
+) -> Result<impl Responder> {
+    use crate::v1::db::schema::contexts::dsl::*;
+
+    let mut conn = state
+        .db_pool
+        .get()
+        .map_err_to_internal_server("Unable to get db connection from pool", Null)?;
+
+    let PaginationParams {
+        page: opt_page,
+        size: opt_size,
+    } = qparams.into_inner();
+    let default_page = 1;
+    let page = opt_page.unwrap_or(default_page);
+    let default_size = 20;
+    let size = opt_size.unwrap_or(default_size);
+
+    if page < 1 {
+        return Err(error::ErrorBadRequest("Param 'page' has to be at least 1."));
+    } else if size < 1 {
+        return Err(error::ErrorBadRequest("Param 'size' has to be at least 1."));
+    }
+
+    let result: Vec<Context> = contexts
+        .order(created_at)
+        .limit(i64::from(size))
+        .offset(i64::from(size * (page - 1)))
+        .load(&mut conn)
+        .map_err_to_internal_server("Failed to execute query, error", Null)?;
+    Ok(web::Json(result))
 }
