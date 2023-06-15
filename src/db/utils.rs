c01d921b8bbe79c466945eefd2a36c8744924921
Shrey Bana <shrey.bana@juspay.in>
2023-06-15 21:01:52 +0530

    fix: PICAF-22967: DB password URI encoded.

--- src/db/utils.rs
+++ src/db/utils.rs
@@ -3,6 +3,7 @@ use diesel::{
     r2d2::{ConnectionManager, Pool},
     PgConnection,
 };
+use urlencoding::encode;
 
 use crate::v1::{aws::kms, helpers::get_from_env_unsafe};
 
@@ -29,7 +30,8 @@ pub async fn get_pool() -> Pool<ConnectionManager<PgConnection>> {
 async fn get_database_url() -> String {
     let db_user: String = get_from_env_unsafe("DB_USER").unwrap();
     let kms_client = kms::new_client();
-    let db_password = kms::decrypt(kms_client, "DB_PASSWORD").await;
+    let db_password_raw = kms::decrypt(kms_client, "DB_PASSWORD").await;
+    let db_password = encode(db_password_raw.as_str()).to_string();
     let db_host: String = get_from_env_unsafe("DB_HOST").unwrap();
     let db_name: String = get_from_env_unsafe("DB_NAME").unwrap();
     format!("postgres://{db_user}:{db_password}@{db_host}/{db_name}")
