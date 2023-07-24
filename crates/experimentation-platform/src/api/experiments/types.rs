60bf5c0e7538cfd52d0613546cc6a061ecbfe7c9
Shubhranshu Sanjeev <shubhranshu.sanjeev@juspay.in>
2023-07-24 19:45:34 +0530

    refactor: improvements to APIs

--- crates/experimentation-platform/src/api/experiments/types.rs
+++ crates/experimentation-platform/src/api/experiments/types.rs
@@ -1,7 +1,12 @@
-use chrono::{DateTime, Utc};
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use std::fmt;
+
 use crate::db::models;
+use chrono::{DateTime, Utc};
+use serde::{
+    de::{self, IntoDeserializer},
+    Deserialize, Serialize,
+};
+use serde_json::Value;
 
 use crate::db::models::ExperimentStatusType;
 
@@ -19,7 +24,7 @@ pub struct Variant {
 }
 
 #[derive(Deserialize)]
-pub struct ExperimentCreateReq {
+pub struct ExperimentCreateRequest {
     pub name: String,
     pub override_keys: Vec<String>,
     pub traffic_percentage: i32,
@@ -29,16 +34,52 @@ pub struct ExperimentCreateReq {
 }
 
 #[derive(Serialize)]
-pub struct ExperimentCreateRes {
+pub struct ExperimentCreateResponse {
     pub message: String,
     pub data: models::Experiment,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct ListFilters {
+    #[serde(deserialize_with = "deserialize_stringified_list")]
     pub status: Vec<ExperimentStatusType>,
     pub from_date: DateTime<Utc>,
     pub to_date: DateTime<Utc>,
     pub page: i64,
     pub count: i64,
+}
+
+pub fn deserialize_stringified_list<'de, D, I>(
+    deserializer: D,
+) -> std::result::Result<Vec<I>, D::Error>
+where
+    D: de::Deserializer<'de>,
+    I: de::DeserializeOwned,
+{
+    struct StringVecVisitor<I>(std::marker::PhantomData<I>);
+
+    impl<'de, I> de::Visitor<'de> for StringVecVisitor<I>
+    where
+        I: de::DeserializeOwned,
+    {
+        type Value = Vec<I>;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a string containing a list")
+        }
+
+        fn visit_str<E>(self, v: &str) -> std::result::Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            let mut query_vector = Vec::new();
+            for param in v.split(",") {
+                let p: I = I::deserialize(param.into_deserializer())?;
+                query_vector.push(p);
+            }
+            Ok(query_vector)
+        }
+    }
+
+    deserializer.deserialize_any(StringVecVisitor(std::marker::PhantomData::<I>))
 }
