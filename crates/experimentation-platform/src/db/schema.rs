1be82f1d33b269135713bf69b3ff9b9d58bea4c9
Shubhranshu Sanjeev <shubhranshu.sanjeev@juspay.in>
2023-08-01 18:55:40 +0530

    fix: moved tables and types under cac_v1 schema

--- crates/experimentation-platform/src/db/schema.rs
+++ crates/experimentation-platform/src/db/schema.rs
@@ -1,25 +1,75 @@
 // @generated automatically by Diesel CLI.
 
-pub mod sql_types {
-    #[derive(diesel::sql_types::SqlType)]
-    #[diesel(postgres_type(name = "experiment_status_type"))]
-    pub struct ExperimentStatusType;
-}
+pub mod cac_v1 {
+    pub mod sql_types {
+        #[derive(diesel::sql_types::SqlType)]
+        #[diesel(postgres_type(name = "dimension_type", schema = "cac_v1"))]
+        pub struct DimensionType;
 
-diesel::table! {
-    use diesel::sql_types::*;
-    use super::sql_types::ExperimentStatusType;
-
-    experiments (id) {
-        id -> Int8,
-        created_at -> Timestamptz,
-        created_by -> Text,
-        last_modified -> Nullable<Timestamptz>,
-        name -> Text,
-        override_keys -> Array<Text>,
-        status -> ExperimentStatusType,
-        traffic_percentage -> Int4,
-        context -> Json,
-        variants -> Json,
+        #[derive(diesel::sql_types::SqlType)]
+        #[diesel(postgres_type(name = "experiment_status_type", schema = "cac_v1"))]
+        pub struct ExperimentStatusType;
     }
+
+    diesel::table! {
+        cac_v1.contexts (id) {
+            id -> Varchar,
+            value -> Json,
+            override_id -> Varchar,
+            created_at -> Timestamptz,
+            created_by -> Varchar,
+            priority -> Int4,
+            #[sql_name = "override"]
+            override_ -> Json,
+        }
+    }
+
+    diesel::table! {
+        cac_v1.default_configs (key) {
+            key -> Varchar,
+            value -> Json,
+            created_at -> Timestamptz,
+            created_by -> Varchar,
+            schema -> Json,
+        }
+    }
+
+    diesel::table! {
+        use diesel::sql_types::*;
+        use super::sql_types::DimensionType;
+
+        cac_v1.dimensions (dimension) {
+            dimension -> Varchar,
+            priority -> Int4,
+            #[sql_name = "type"]
+            type_ -> DimensionType,
+            created_at -> Timestamptz,
+            created_by -> Varchar,
+        }
+    }
+
+    diesel::table! {
+        use diesel::sql_types::*;
+        use super::sql_types::ExperimentStatusType;
+
+        cac_v1.experiments (id) {
+            id -> Int8,
+            created_at -> Timestamptz,
+            created_by -> Text,
+            last_modified -> Nullable<Timestamptz>,
+            name -> Text,
+            override_keys -> Array<Text>,
+            status -> ExperimentStatusType,
+            traffic_percentage -> Int4,
+            context -> Json,
+            variants -> Json,
+        }
+    }
+
+    diesel::allow_tables_to_appear_in_same_query!(
+        contexts,
+        default_configs,
+        dimensions,
+        experiments,
+    );
 }
