37179538d1c5f8d67424d77fcde02cdee3122129
Ritick Madaan <ritickmadaan57@gmail.com>
2023-08-28 17:19:04 +0530

    feat:  implemented tracing-actix-web for logging

--- crates/context-aware-config/src/helpers.rs
+++ crates/context-aware-config/src/helpers.rs
@@ -1,5 +1,7 @@
+use actix_web::http::header::{HeaderMap, HeaderName, HeaderValue};
 use jsonschema::{Draft, JSONSchema};
 use serde_json::json;
+use std::collections::HashMap;
 
 pub fn get_default_config_validation_schema() -> JSONSchema {
     let my_schema = json!({
@@ -37,4 +39,23 @@ pub fn get_default_config_validation_schema() -> JSONSchema {
         .with_draft(Draft::Draft7)
         .compile(&my_schema)
         .expect("THE IMPOSSIBLE HAPPENED, failed to compile the schema for the schema!")
+}
+
+pub fn parse_headermap_safe(headermap: &HeaderMap) -> HashMap<String, String> {
+    let mut req_headers = HashMap::new();
+    let record_header = |(header_name, header_val): (&HeaderName, &HeaderValue)| {
+        let header_val = match header_val.to_str() {
+            Ok(s) => String::from(s),
+            Err(e) => {
+                log::error!(
+                    "unable to parse value of header {}, error: {e}",
+                    header_name
+                );
+                String::from("Error: non ASCII header value")
+            }
+        };
+        req_headers.insert(header_name.to_string(), header_val);
+    };
+    headermap.iter().for_each(record_header);
+    req_headers
 }
