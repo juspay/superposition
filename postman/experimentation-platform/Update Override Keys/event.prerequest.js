5121816f609623b764faa9262a3f32ec6118e1b4
Shubhranshu Sanjeev <shubhranshu.sanjeev@juspay.in>
2023-09-04 21:30:52 +0530

    test: added postman test for update override_keys api

---
+++ postman/experimentation-platform/Update Override Keys/event.prerequest.js
@@ -0,0 +1,41 @@
+const host = pm.environment.get("host");
+const token = pm.environment.get("token");
+
+function create_default_config_keys() {
+    let keys = [
+        `pmTestKey790`,
+        `pmTestKey690`
+    ];
+
+    for (const key of keys) {
+        const options = {
+            'method': 'PUT',
+            'url': `${host}/default-config/${key}`,
+            "header": {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            "body": {
+                "mode": "raw",
+                "raw": JSON.stringify({
+                    "value": "value1",
+                    "schema": {
+                        "type": "string",
+                        "pattern": ".*"
+                    }
+                })
+            }
+        };
+        console.log(options);
+        pm.sendRequest(options, function (error, response) {
+            if (error) {
+                console.log(`Error creating default-config key: ${key}`);
+                console.log(error);
+                return;
+            };
+            console.log(`Created default-config key: ${key}`);
+        });
+    }
+}
+
+create_default_config_keys()
