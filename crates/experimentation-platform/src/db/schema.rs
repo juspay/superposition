c249efc4a15482bbad3efcafa7fe8352c24735e2
Shubhranshu Sanjeev <shubhranshu.sanjeev@juspay.in>
2023-09-08 12:44:02 +0530

    fix: failed build due to untracked schema.rs file changes

--- crates/experimentation-platform/src/db/schema.rs
+++ crates/experimentation-platform/src/db/schema.rs
@@ -3,10 +3,6 @@
 pub mod cac_v1 {
     pub mod sql_types {
         #[derive(diesel::sql_types::SqlType)]
-        #[diesel(postgres_type(name = "dimension_type", schema = "cac_v1"))]
-        pub struct DimensionType;
-
-        #[derive(diesel::sql_types::SqlType)]
         #[diesel(postgres_type(name = "experiment_status_type", schema = "cac_v1"))]
         pub struct ExperimentStatusType;
     }
@@ -35,16 +31,12 @@ pub mod cac_v1 {
     }
 
     diesel::table! {
-        use diesel::sql_types::*;
-        use super::sql_types::DimensionType;
-
         cac_v1.dimensions (dimension) {
             dimension -> Varchar,
             priority -> Int4,
-            #[sql_name = "type"]
-            type_ -> DimensionType,
             created_at -> Timestamptz,
             created_by -> Varchar,
+            schema -> Json,
         }
     }
 
