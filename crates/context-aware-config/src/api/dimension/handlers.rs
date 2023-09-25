3104c5b815580fd78a9d0f155648dcacd63cea21
Kartik Gajendra <kartik.gajendra@juspay.in>
2023-09-25 13:28:05 +0530

    feat:  added dashboard auth middleware
    - Validates and authorizes users based on dashboard ACLs
    - Validates tenant ID in header
    - Adds an ACL guard for services

--- crates/context-aware-config/src/api/dimension/handlers.rs
+++ crates/context-aware-config/src/api/dimension/handlers.rs
@@ -9,50 +9,52 @@ use actix_web::{
     HttpResponse, Scope,
 };
 use chrono::Utc;
+use dashboard_auth::{
+    middleware::acl,
+    types::User,
+};
 use diesel::RunQueryDsl;
-use service_utils::service::types::{AppState, AuthenticationInfo};
 use jsonschema::{Draft, JSONSchema};
+use service_utils::service::types::AppState;
 
 pub fn endpoints() -> Scope {
-    Scope::new("").service(create)
+    Scope::new("")
+        .guard(acl([("mjos_manager".into(), "RW".into())]))
+        .service(create)
 }
 
 #[put("")]
 async fn create(
     state: Data<AppState>,
     req: web::Json<CreateReq>,
-    auth_info: AuthenticationInfo,
+    user: User,
 ) -> HttpResponse {
     //TODO move this to the type itself rather than special if check
     if req.priority <= 0 {
         return HttpResponse::BadRequest().body("Priority should be greater than 0");
     }
 
-    let AuthenticationInfo(email) = auth_info;
-
     let create_req = req.into_inner();
     let schema_value = create_req.schema;
 
-    if let Err(e) =
-        validate_jsonschema(&state.meta_schema, &schema_value)
-    {
+    if let Err(e) = validate_jsonschema(&state.meta_schema, &schema_value) {
         return HttpResponse::BadRequest().body(e);
     };
-
 
     let schema_compile_result = JSONSchema::options()
         .with_draft(Draft::Draft7)
         .compile(&schema_value);
 
     if let Err(e) = schema_compile_result {
-        return HttpResponse::BadRequest().body(String::from(format!("Bad schema: {:?}", e )));
+        return HttpResponse::BadRequest()
+            .body(String::from(format!("Bad schema: {:?}", e)));
     };
 
     let new_dimension = Dimension {
         dimension: create_req.dimension,
         priority: i32::from(create_req.priority),
         schema: schema_value,
-        created_by: email,
+        created_by: user.email,
         created_at: Utc::now(),
     };
 
