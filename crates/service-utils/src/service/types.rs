9a0360dc91e6916e1b1432b8f6f2ef9e588d632f
Kartik Gajendra <kartik.gajendra@juspay.in>
2023-10-05 15:12:13 +0530

    fix: [PICAF-24563] add user struct in delete context API

--- crates/service-utils/src/service/types.rs
+++ crates/service-utils/src/service/types.rs
@@ -28,57 +28,6 @@ pub struct AppState {
     pub snowflake_generator: Mutex<SnowflakeIdGenerator>,
 }
 
-#[derive(Clone)]
-pub struct AuthenticationInfo(pub String);
-impl FromRequest for AuthenticationInfo {
-    type Error = Error;
-    type Future = Ready<Result<Self, Self::Error>>;
-
-    fn from_request(
-        req: &actix_web::HttpRequest,
-        _: &mut actix_web::dev::Payload,
-    ) -> Self::Future {
-        let opt_token = req
-            .headers()
-            .get("Authorization")
-            .and_then(|h| h.to_str().ok())
-            .and_then(|h| {
-                if h.starts_with("Bearer") {
-                    Some(h)
-                } else {
-                    None
-                }
-            })
-            .and_then(|h| {
-                h.split(' ')
-                    .collect::<Vec<_>>()
-                    .get(1)
-                    .map(|token| token.to_string())
-            });
-
-        let opt_admin_token = req
-            .app_data()
-            .map(|d: &Data<AppState>| d.admin_token.as_str());
-
-        let result = match (opt_token, opt_admin_token) {
-            (_, None) => {
-                log::info!("ERROR: ADMIN TOKEN NOT FOUND!!!!");
-                Err(error::ErrorInternalServerError(""))
-            }
-            (None, _) => Err(error::ErrorUnauthorized("Bearer token required.")),
-            (Some(token), Some(admin_token)) if token != admin_token => {
-                Err(error::ErrorUnauthorized(""))
-            }
-            (Some(_token), Some(_admin_token)) => {
-                let email = "cac.admin@juspay.in";
-                let auth_info = AuthenticationInfo(email.to_string());
-                Ok(auth_info)
-            }
-        };
-        ready(result)
-    }
-}
-
 pub struct DbConnection(pub PooledConnection<ConnectionManager<PgConnection>>);
 impl FromRequest for DbConnection {
     type Error = Error;
