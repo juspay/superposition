4db2c31f923d2298df879f9251a7f08552f26d65
Kartik Gajendra <kartik.gajendra@juspay.in>
2023-11-16 18:04:45 +0530

    feat: support more operations

--- crates/caclang/src/bin.rs
+++ crates/caclang/src/bin.rs
@@ -13,6 +13,7 @@ struct Args {
 }
 
 pub fn main() -> anyhow::Result<()> {
+    env_logger::init();
     let args = Args::parse();
     let config = fs::read_to_string(args.file)?;
     let cac = ContextAwareConfig::parse(config)?;
@@ -20,39 +21,33 @@ pub fn main() -> anyhow::Result<()> {
         let mut context = HashMapContext::new();
         for (dimension, props) in cac.dimensions.iter() {
             if props.data_type == DataType::String {
-                if let Some(user_value) =
-                    Text::new(format!("Value for {dimension}, hit Esc key to skip").as_str())
-                        .prompt_skippable()?
+                if let Some(user_value) = Text::new(
+                    format!("Value for {dimension}, hit Esc key to skip").as_str(),
+                )
+                .prompt_skippable()?
                 {
                     context.set_value(dimension.clone(), user_value.into())?;
                 }
             } else if props.data_type == DataType::Number {
-                if let Some(user_value) =
-                    CustomType::<i64>::new(format!("Value for {dimension}, hit Esc key to skip").as_str())
-                        .prompt_skippable()?
+                if let Some(user_value) = CustomType::<i64>::new(
+                    format!("Value for {dimension}, hit Esc key to skip").as_str(),
+                )
+                .prompt_skippable()?
                 {
                     context.set_value(dimension.clone(), user_value.into())?;
                 }
             } else {
-                if let Some(user_value) =
-                    CustomType::<bool>::new(format!("Value for {dimension}, hit Esc key to skip").as_str())
-                        .prompt_skippable()?
+                if let Some(user_value) = CustomType::<bool>::new(
+                    format!("Value for {dimension}, hit Esc key to skip").as_str(),
+                )
+                .prompt_skippable()?
                 {
                     context.set_value(dimension.clone(), user_value.into())?;
                 }
             };
         }
-        for ctx in cac.contexts.iter() {
-            let eval = ctx.expr.eval_with_context(&context);
-            if eval.is_ok() && eval?.as_boolean()? {
-                println!("============================================");
-                println!("context {}", ctx.context);
-                println!("Expression {:?}", ctx.expr.to_string());
-                println!("overrides {:?}", ctx.overrides);
-                println!("priority {:?}", ctx.calculated_priority);
-                println!("============================================");
-            }
-        }
+        let v = cac.get_config("android_hyperpay_configuration", &context)?;
+        println!("config value: {v}");
         if CustomType::<bool>::new("Hit Esc to stop, enter to continue")
             .prompt_skippable()?
             .is_none()
