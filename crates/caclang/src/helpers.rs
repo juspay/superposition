2c3d0dcf7b925279efa52e4fe38dcf4700aaf73d
Kartik Gajendra <kartik.gajendra@juspay.in>
2023-11-16 18:04:45 +0530

    feat: support more operations

--- crates/caclang/src/helpers.rs
+++ crates/caclang/src/helpers.rs
@@ -20,6 +20,40 @@ pub(crate) fn extract_section(
     Ok(section.clone())
 }
 
+pub fn compute_cac_hash(ctx: &String) -> anyhow::Result<String> {
+    let tokens = ctx.split(AND_TOKEN).collect::<Vec<&str>>();
+    let mut final_tokens: Vec<String> = Vec::new();
+    for token in tokens.into_iter() {
+        let mut segments = token.trim().split(" ");
+        let (dimension, op, value) = (
+            segments
+                .next()
+                .ok_or(anyhow!("Dimension not found for {ctx} in rule {token}"))?,
+            segments
+                .next()
+                .ok_or(anyhow!("Operator not found for {ctx} in rule {token}"))?,
+            segments
+                .next()
+                .ok_or(anyhow!("Value not found for {ctx} in rule {token}"))?,
+        );
+        if op == "IN" {
+            let mut list_items =
+                value[1..value.len() - 1].split(",").collect::<Vec<&str>>();
+            list_items.sort();
+            let final_list = list_items.join(",");
+            final_tokens.push(format!("{dimension} {op} [{final_list}]"));
+        } else {
+            final_tokens.push(String::from(token));
+        }
+    }
+    final_tokens.sort();
+    let to_be_hashed = final_tokens.join(",").to_string();
+    log::debug!("{}", to_be_hashed);
+    let hash = blake3::hash(to_be_hashed.as_bytes()).to_string();
+    log::debug!("hash: {}", hash);
+    Ok(hash)
+}
+
 pub(crate) fn parse_and_validate_ctx(
     dimensions: &Dimensions,
     ctx: &String,
@@ -64,7 +98,11 @@ pub(crate) fn parse_and_validate_ctx(
                 }
             };
 
-        let expr_rule = match operator {
+        let expr_rule = match operator.to_lowercase().as_str() {
+            "is" => {
+                validator(value)?;
+                gen_expr("==", dimension, value, &props.data_type)
+            }
             "=" => {
                 validator(value)?;
                 gen_expr("==", dimension, value, &props.data_type)
@@ -73,15 +111,27 @@ pub(crate) fn parse_and_validate_ctx(
                 validator(value)?;
                 gen_expr(">=", dimension, value, &props.data_type)
             }
+            ">" => {
+                validator(value)?;
+                gen_expr(">", dimension, value, &props.data_type)
+            }
             "<=" => {
                 validator(value)?;
                 gen_expr("<=", dimension, value, &props.data_type)
+            }
+            "<" => {
+                validator(value)?;
+                gen_expr("<", dimension, value, &props.data_type)
+            }
+            "not" => {
+                validator(value)?;
+                gen_expr("!=", dimension, value, &props.data_type)
             }
             "!=" => {
                 validator(value)?;
                 gen_expr("!=", dimension, value, &props.data_type)
             }
-            "IN" => {
+            "in" => {
                 let values = &value[1..value.len() - 1];
                 let mut items: Vec<String> = Vec::new();
                 for item in values.split(',').into_iter() {
@@ -90,7 +140,6 @@ pub(crate) fn parse_and_validate_ctx(
                         items.push(format!("\"{item}\""));
                     } else {
                         items.push(format!("{item}"));
-
                     }
                 }
                 gen_expr("IN", dimension, items.join(",").as_str(), &props.data_type)
