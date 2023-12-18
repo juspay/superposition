cacf20f3371dfb3c7de823534bc34978bb18954d
Saurav Suman <saurav.suman@Saurav-Suman-DQGRVM2TW4.local>
2023-12-18 21:31:05 +0530

    feat: added validation inside default config form , formatted dates , added disable feature of edit

--- crates/frontend/src/pages/DefaultConfig/DefaultConfig.rs
+++ crates/frontend/src/pages/DefaultConfig/DefaultConfig.rs
@@ -24,6 +24,18 @@ pub struct RowData {
     pub value: String,
 }
 
+// fn parse_string_to_json_value_vec(input: &str) -> Vec<Value> {
+//     // Parse the input string into a serde_json::Value
+//     let parsed = serde_json::from_str::<Value>(input)
+//         .expect("Failed to parse JSON");
+
+//     // Ensure the Value is an Array and convert it to Vec<Value>
+//     match parsed {
+//         Value::Array(arr) => arr,
+//         _ => panic!("Input is not a JSON array"),
+//     }
+// }
+
 pub async fn fetch_config(tenant: String) -> Result<Config, String> {
     let client = reqwest::Client::new();
     let host = "http://localhost:8080";
@@ -49,10 +61,37 @@ pub async fn create_default_config(
     let url = format!("{host}/default-config/{key}");
     let mut req_body: HashMap<&str, Value> = HashMap::new();
     let mut schema: Map<String, Value> = Map::new();
-    schema.insert("type".to_string(), Value::String(key_type));
-    schema.insert("pattern".to_string(), Value::String(pattern));
-    req_body.insert("value", Value::String(value));
+    match key_type.as_str() {
+        "number" => {
+            schema.insert("type".to_string(), Value::String(key_type.clone()));
+            req_body.insert(
+                "value",
+                Value::Number(value.clone().parse::<i32>().unwrap().into()),
+            );
+        }
+        // "Enum" => {
+        //     let array = parse_string_to_json_value_vec(pattern.clone().as_str());
+        //     schema.insert("type".to_string(),Value::String("string".to_string()));
+        //     schema.insert("enum".to_string(), Value::Array(array));
+        //     req_body.insert("value", Value::Array(value2));
+        // },
+        "Pattern" | "Enum" => {
+            schema.insert("type".to_string(), Value::String("string".to_string()));
+            schema.insert("pattern".to_string(), Value::String(pattern.clone()));
+        }
+        _ => {
+            let json_pattern = serde_json::from_str::<Value>(&pattern.clone())
+                .map_err(|e| e.to_string())?;
+            req_body.insert("schema", json_pattern);
+        }
+    }
+
+    if key_type != "number".to_string() {
+        req_body.insert("value", Value::String(value));
+    }
+
     req_body.insert("schema", Value::Object(schema));
+
     let response = client
         .put(url)
         .header("x-tenant", tenant)
@@ -83,17 +122,23 @@ fn ModalComponent(handle_submit: Rc<dyn Fn()>) -> impl IntoView {
 #[component]
 fn FormComponent(handle_submit: Rc<dyn Fn()>) -> impl IntoView {
     use leptos::html::Input;
+    use leptos::html::Textarea;
     let handle_submit = handle_submit.clone();
     let global_state = use_context::<RwSignal<RowData>>();
     let row_data = global_state.unwrap().get();
 
-    let (key, set_key) = create_signal(row_data.key);
-    let (value, set_value) = create_signal(row_data.value);
-    let (keytype, set_keytype) = create_signal("string".to_string());
-    let (pattern, set_pattern) = create_signal(".*".to_string());
+    let (key, set_key) = create_signal("".to_string());
+    let (value, set_value) = create_signal("".to_string());
+    let (keytype, set_keytype) = create_signal("".to_string());
+    let (pattern, set_pattern) = create_signal("".to_string());
+
+    let edit_signal = use_context::<RwSignal<bool>>();
+
+    let (show_labels, set_show_labels) = create_signal(false);
 
     create_effect(move |_| {
         if let Some(row_data) = global_state {
+            logging::log!("default config create effect");
             set_key.set(row_data.get().key.clone().to_string());
             set_value.set(row_data.get().value.clone().to_string());
         }
@@ -101,8 +146,7 @@ fn FormComponent(handle_submit: Rc<dyn Fn()>) -> impl IntoView {
 
     let input_element: NodeRef<Input> = create_node_ref();
     let input_element_two: NodeRef<Input> = create_node_ref();
-    let input_element_three: NodeRef<Input> = create_node_ref();
-    let input_element_four: NodeRef<Input> = create_node_ref();
+    let input_element_three: NodeRef<Textarea> = create_node_ref();
 
     let on_submit = {
         let handle_submit = handle_submit.clone();
@@ -112,12 +156,11 @@ fn FormComponent(handle_submit: Rc<dyn Fn()>) -> impl IntoView {
             let value1 = input_element.get().expect("<input> to exist").value();
             let value2 = input_element_two.get().expect("<input> to exist").value();
             let value3 = input_element_three.get().expect("<input> to exist").value();
-            let value4 = input_element_four.get().expect("<input> to exist").value();
 
             set_key.set(value1.clone());
             set_value.set(value2.clone());
-            set_keytype.set(value3.clone());
-            set_pattern.set(value4.clone());
+            set_pattern.set(value3.clone());
+
             let handle_submit_clone = handle_submit.clone();
 
             spawn_local({
@@ -148,7 +191,7 @@ fn FormComponent(handle_submit: Rc<dyn Fn()>) -> impl IntoView {
 
     view! {
         <form
-            class="form-control w-full space-y-4 bg-white text-gray-700 font-mono"
+            class="form-control w-full overflow-auto space-y-4 bg-white text-gray-700 font-mono"
             on:submit=on_submit
         >
             <div class="form-control">
@@ -156,6 +199,7 @@ fn FormComponent(handle_submit: Rc<dyn Fn()>) -> impl IntoView {
                     <span class="label-text text-gray-700 font-mono">Key</span>
                 </label>
                 <input
+                    disabled = move || {edit_signal.unwrap().get()}
                     type="text"
                     placeholder="Key"
                     class="input input-bordered w-full bg-white text-gray-700 shadow-md"
@@ -163,66 +207,119 @@ fn FormComponent(handle_submit: Rc<dyn Fn()>) -> impl IntoView {
                     node_ref=input_element
                 />
             </div>
-            <div class="form-control">
-                <label class="label font-mono">
-                    <span class="label-text text-gray-700 font-mono">Value</span>
-                </label>
-                <input
-                    type="text"
-                    placeholder="Value"
-                    class="input input-bordered w-full bg-white text-gray-700 shadow-md"
-                    value=value
-                    node_ref=input_element_two
-                />
+
+
+            <div tabindex = "0" class="dropdown dropdown-bottom">
+            <div tabindex="0" role="button" class="btn m-1">
+            <i class="ri-arrow-down-s-line"></i>
+            Add Schema
             </div>
-            <div class="form-control">
-                <label class="label font-mono">
-                    <span class="label-text text-gray-700 font-mono">Type</span>
-                </label>
-                <input
-                    type="text"
-                    placeholder="Type"
-                    class="input input-bordered w-full bg-white text-gray-700 shadow-md"
-                    value=keytype
-                    node_ref=input_element_three
-                />
+
+            <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100
+            rounded-box w-52">
+                <li on:click = move |_| {
+                    set_show_labels.set(true);
+                    set_keytype.set("number".to_string());
+                }>
+                <a>"Number"</a>
+                </li>
+                <li on:click = move |_| {
+                    set_show_labels.set(true);
+                    set_keytype.set("Enum".to_string());
+                    set_pattern.set(format!("{:?}", vec!["android", "web", "ios"]));
+                }>
+                <a>"String (Enum)"</a>
+                </li>
+                <li on:click = move |_| {
+                    set_show_labels.set(true);
+                    set_keytype.set("Pattern".to_string());
+                    set_pattern.set(".*".to_string());
+                }>
+                <a>"String (Regex)"</a>
+                </li>
+                <li on:click = move |_| {
+                    set_show_labels.set(true);
+                    set_keytype.set("Other".to_string());
+                    set_pattern.set("".to_string());
+                }>
+                <a>"Other"</a>
+                </li>
+            </ul>
             </div>
-            <div class="form-control">
-                <label class="label font-mono">
-                    <span class="label-text text-gray-700 font-mono">Pattern (regex)</span>
-                </label>
-                <input
-                    type="text"
-                    placeholder="Pattern"
-                    class="input input-bordered w-full bg-white text-gray-700 shadow-md"
-                    value=pattern
-                    node_ref=input_element_four
-                />
-            </div>
-            <div class="form-control mt-6">
-                <Button text="Submit".to_string() on_click=|_| modal_action("my_modal_5", "close")/>
-            </div>
-        </form>
+            {
+              move || {
+                view!{
+                  <Show when = move || (keytype.get() == "number")>
+                  <div class="form-control">
+                  <label class="label font-mono">
+                      <span class="label-text text-gray-700 font-mono">Value</span>
+                  </label>
+                  <input
+                      type="number"
+                      placeholder="Value"
+                      class="input input-bordered w-full bg-white text-gray-700 shadow-md"
+                      value=value
+                      node_ref=input_element_two
+                  />
+              </div>
+              </Show>
+
+                  <Show when = move || (show_labels.get() && (keytype.get() != "number")) >
+                  <div class="form-control">
+                  <label class="label font-mono">
+                      <span class="label-text text-gray-700 font-mono">Value</span>
+                  </label>
+                  <input
+                      type="text"
+                      placeholder="Value"
+                      class="input input-bordered w-full bg-white text-gray-700 shadow-md"
+                      value=value
+                      node_ref=input_element_two
+                  />
+              </div>
+                        <div class="form-control">
+                        <label class="label font-mono">
+                        <span class="label-text text-gray-700 font-mono">{keytype.get()}</span>
+                        </label>
+                            <textarea
+                              type = "text"
+                              class="input input-bordered w-full bg-white text-gray-700 shadow-md"
+
+                              node_ref=input_element_three
+                            > {pattern.get()}
+                            </textarea>
+
+                    </div>
+                   </Show>
+                }
+              }
+            }
+                    <div class="form-control mt-6">
+                    <Button text="Submit".to_string() on_click= |_| modal_action("my_modal_5","close") />
+                </div>
+            </form>
     }
 }
 
-fn custom_formatter(value: &str, row: &Map<String, Value>) -> View {
-    let intermediate_signal = use_context::<RwSignal<Option<RowData>>>().unwrap();
+fn custom_formatter(_value: &str, row: &Map<String, Value>) -> View {
+    let global_signal = use_context::<RwSignal<RowData>>().unwrap();
     let row_key = row["key"].clone().to_string().replace("\"", "");
     let row_value = row["value"].clone().to_string().replace("\"", "");
 
+    let edit_signal = use_context::<RwSignal<bool>>().unwrap();
     let edit_click_handler = move |_| {
         let row_data = RowData {
             key: row_key.clone(),
             value: row_value.clone(),
         };
-        intermediate_signal.set(Some(row_data));
+        edit_signal.set(true);
+        global_signal.set(row_data);
         js_sys::eval("document.getElementById('my_modal_5').showModal();").unwrap();
     };
 
-    let edit_icon: HtmlElement<html::I> = view! { <i class="ri-pencil-line ri-xl text-blue-500" on:click=edit_click_handler></i> };
+    let edit_icon: HtmlElement<html::I> = view! { <i class="ri-pencil-line ri-xl text-blue-500"></i> };
 
-    view! { <span>{edit_icon}</span> }.into_view()
+    view! { <span on:click = edit_click_handler>{edit_icon}</span> }.into_view()
 }
 
 #[component]
@@ -231,16 +328,8 @@ pub fn DefaultConfig() -> impl IntoView {
     let global_state = create_rw_signal(RowData::default());
     provide_context(global_state);
 
-    let intermediate_signal = create_rw_signal(None::<RowData>);
-
-    // Listener for intermediate signal
-    create_effect(move |_| {
-        if let Some(row_data) = intermediate_signal.get() {
-            global_state.set(row_data.clone());
-        }
-    });
-
-    provide_context(intermediate_signal.clone());
+    let edit_signal = create_rw_signal(true);
+    provide_context(edit_signal);
 
     let query = use_query_map();
 
@@ -275,51 +364,65 @@ pub fn DefaultConfig() -> impl IntoView {
     view! {
         <div class="p-8">
             <ModalComponent handle_submit=Rc::new(move || default_config_resource.refetch())/>
-            <Suspense fallback=move || {
-                view! { <p>"Loading (Suspense Fallback)..."</p> }
-            }>
+            <Suspense
+                fallback=move || {
+                    view! { <p>"Loading (Suspense Fallback)..."</p> }
+                }
+            >
+                {
 
-                {move || {
-                    let default_config = default_config_resource.get().unwrap_or(vec![]);
-                    let total_default_config_keys = default_config.len().to_string();
-                    let table_settings = TableSettings {
-                        redirect_prefix: None,
-                    };
-                    let table_rows = default_config
-                        .into_iter()
-                        .map(|config| { json!(config).as_object().unwrap().to_owned() })
-                        .collect::<Vec<Map<String, Value>>>();
-                    view! {
-                        <div class="pb-4">
-                            <Stat
-                                heading="Config Keys"
-                                icon="ri-tools-line"
-                                number=total_default_config_keys
-                            />
-                        </div>
-                        <div class="card rounded-lg w-full bg-base-100 shadow">
-                            <div class="card-body">
-                                <div class="flex justify-between">
-                                    <h2 class="card-title chat-bubble text-gray-800 dark:text-white bg-white font-mono">
-                                        "Default Config"
-                                    </h2>
-                                    <Button
-                                        text="Create Default Config".to_string()
-                                        on_click=|_| modal_action("my_modal_5", "open")
-                                    />
-                                </div>
-                                <Table
-                                    table_style="font-mono".to_string()
-                                    rows=table_rows
-                                    key_column="id".to_string()
-                                    columns=table_columns.get()
-                                    settings=table_settings
+                    let edit_signal = edit_signal.clone();
+                    move || {
+                        let default_config = default_config_resource.get().unwrap_or(vec![]);
+                        let total_default_config_keys = default_config.len().to_string();
+                        let table_settings = TableSettings {
+                            redirect_prefix: None
+                        };
+
+                        let edit_signal = edit_signal.clone();
+
+                        let table_rows = default_config
+                            .into_iter()
+                            .map(|config| {
+                                let mut ele_map = json!(config).as_object().unwrap().to_owned();
+                                ele_map.insert("created_at".to_string(),json!(config.created_at.format("%v").to_string()));
+                                ele_map
+                            })
+                            .collect::<Vec<Map<String, Value>>>();
+
+                        view! {
+                            <div class="pb-4">
+                                <Stat
+                                    heading="Config Keys"
+                                    icon="ri-tools-line"
+                                    number={total_default_config_keys}
                                 />
                             </div>
-                        </div>
+                            <div class="card rounded-lg w-full bg-base-100 shadow">
+                                <div class="card-body">
+                                    <div class="flex justify-between">
+                                        <h2 class="card-title chat-bubble text-gray-800 dark:text-white bg-white font-mono">
+                                            "Default Config"
+                                        </h2>
+                                        <Button text="Create Key".to_string() on_click= {
+                                            let edit_signal_clone = edit_signal.to_owned();
+                                            move |_| {
+                                            edit_signal_clone.set(false);
+                                            modal_action("my_modal_5","open");
+                                        }}/>
+                                    </div>
+                                    <Table
+                                        table_style="font-mono".to_string()
+                                        rows=table_rows
+                                        key_column="id".to_string()
+                                        columns=table_columns.get()
+                                        settings=table_settings
+                                    />
+                                </div>
+                            </div>
+                        }
                     }
-                }}
-
+                }
             </Suspense>
         </div>
     }
