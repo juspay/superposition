b6043ce4da68b91d3c6d5ad464cb1b139fabea5c
Akhilesh Bhadauriya <akhilesh.b@juspay.in>
2024-01-05 17:31:10 +0530

    feat: added between in frontend

--- crates/frontend/src/components/condition_pills/condition_pills.rs
+++ crates/frontend/src/components/condition_pills/condition_pills.rs
@@ -11,6 +11,7 @@ pub fn context_pills(context: Value) -> impl IntoView {
         {ctx_values
             .into_iter()
             .map(|(dim, op, val)| {
+                let operator = op.clone();
                 view! {
                     <span class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs ring-1 ring-inset ring-purple-700/10 shadow-md gap-x-2">
                         <span class="font-mono font-medium context_condition text-gray-500">
@@ -19,7 +20,39 @@ pub fn context_pills(context: Value) -> impl IntoView {
                         <span class="font-mono font-medium text-gray-650 context_condition ">
                             {op}
                         </span>
-                        <span class="font-mono font-semibold context_condition">{val}</span>
+
+                        {match operator.trim() {
+                            "BETWEEN" => {
+                                let split_val: Vec<String> = val
+                                    .clone()
+                                    .split(",")
+                                    .map(String::from)
+                                    .collect();
+                                view! {
+                                    <>
+                                        <span class="font-mono font-semibold context_condition">
+                                            {&split_val[0]}
+                                        </span>
+                                        <span class="font-mono font-medium text-gray-650 context_condition ">
+                                            {"and"}
+                                        </span>
+                                        <span class="font-mono font-semibold context_condition">
+                                            {&split_val[1]}
+                                        </span>
+                                    </>
+                                }
+                            }
+                            _ => {
+                                view! {
+                                    <>
+                                        <span class="font-mono font-semibold context_condition">
+                                            {val}
+                                        </span>
+                                    </>
+                                }
+                            }
+                        }}
+
                     </span>
                 }
             })
