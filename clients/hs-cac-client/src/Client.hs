85751e00e9ec0d4e3719e373a79b3886c7b06f9b
Natarajan Kannan <natarajan@juspay.in>
2024-02-08 19:25:04 +0530

    docs:  add intro doc and features
    feat: added HS cac client
    
    fix: a time bug with last_modified
    
    feat: added experimentation client
    
    feat: a working nix build
    
    feat: migrate from naersk to crane
    
    feat: flake parts added
    
    feat: nixified hs-cac-client
    
    feat: completed nixification

---
+++ clients/hs-cac-client/src/Client.hs
@@ -0,0 +1,121 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
+
+module Client
+( createCacClient
+, getCacClient
+, getCacConfig
+, getCacLastModified
+, cacEval
+, cacStartPolling
+) where
+
+import           Data.Aeson
+import           Data.Functor          (($>))
+import           Foreign.C.String      (CString, newCAString, peekCAString)
+import           Foreign.C.Types       (CInt (CInt), CULong (..))
+import           Foreign.ForeignPtr
+import           Foreign.Marshal.Alloc (free)
+import           Foreign.Ptr
+import           Prelude
+
+data Arc_Client
+
+type CacClient = Arc_Client
+
+type CTenant = CString
+type Tenant = String
+
+type Error = String
+
+foreign import ccall unsafe "new_client"
+    c_new_cac_client :: CTenant -> Bool -> CULong -> CString -> IO CInt
+
+foreign import ccall unsafe "&free_client"
+    c_free_cac_client :: FunPtr (Ptr CacClient -> IO ())
+
+foreign import ccall unsafe "get_client"
+    c_get_cac_client :: CTenant -> IO (Ptr CacClient)
+
+foreign import ccall unsafe "last_error_message"
+    c_last_error_message :: IO CString
+
+foreign import ccall unsafe "get_last_modified"
+    c_get_last_modified_time :: Ptr CacClient -> IO CString
+
+foreign import ccall unsafe "get_config"
+    c_get_config :: Ptr CacClient -> IO CString
+
+foreign import ccall unsafe "cac_eval"
+    c_cac_eval :: Ptr CacClient -> CString -> IO CString
+
+foreign import ccall safe "start_polling_update"
+    c_cac_poll :: CTenant -> IO ()
+
+foreign import ccall unsafe "&free_string"
+    c_free_string :: FunPtr (CString -> IO ())
+
+cacStartPolling :: Tenant -> IO ()
+cacStartPolling tenant =
+    newCAString tenant
+    >>= newForeignPtr c_free_string
+    >>= flip withForeignPtr c_cac_poll
+
+getError :: IO String
+getError = c_last_error_message
+            >>= newForeignPtr c_free_string
+            >>= flip withForeignPtr peekCAString
+
+cleanup :: [Ptr a] -> IO ()
+cleanup items = mapM free items $> ()
+
+createCacClient:: Tenant -> Bool -> Integer -> String -> IO (Either Error ())
+createCacClient tenant shouldUpdate frequency hostname = do
+    let duration = fromInteger frequency
+    cTenant   <- newCAString tenant
+    cHostname <- newCAString hostname
+    resp      <- c_new_cac_client cTenant shouldUpdate duration cHostname
+    _         <- cleanup [cTenant, cHostname]
+    case resp of
+        0 -> pure $ Right ()
+        _ -> Left <$> getError
+
+getCacClient :: Tenant -> IO (Either Error (ForeignPtr CacClient))
+getCacClient tenant = do
+    cTenant   <- newCAString tenant
+    cacClient <- c_get_cac_client cTenant
+    _         <- cleanup [cTenant]
+    if cacClient == nullPtr
+        then Left <$> getError
+        else Right <$> newForeignPtr c_free_cac_client cacClient
+
+getCacConfig :: ForeignPtr CacClient -> IO (Either Error Value)
+getCacConfig client = do
+    config <- withForeignPtr client c_get_config
+    if config == nullPtr
+        then Left <$> getError
+        else do
+            fptrConfig <- newForeignPtr c_free_string config
+            Right . toJSON <$> withForeignPtr fptrConfig peekCAString
+
+getCacLastModified :: ForeignPtr CacClient -> IO (Either Error String)
+getCacLastModified client = do
+    lastModified <- withForeignPtr client c_get_last_modified_time
+    if lastModified == nullPtr
+        then Left <$> getError
+        else do
+            fptrLastModified <- newForeignPtr c_free_string lastModified
+            Right <$> withForeignPtr fptrLastModified peekCAString
+
+cacEval :: ForeignPtr CacClient -> String -> IO (Either Error Value)
+cacEval client context = do
+    cContext  <- newCAString context
+    overrides <- withForeignPtr client (`c_cac_eval` cContext)
+    _         <- cleanup [cContext]
+    if overrides == nullPtr
+        then Left <$> getError
+        else do
+            fptrOverrides <- newForeignPtr c_free_string overrides
+            Right . toJSON <$> withForeignPtr fptrOverrides peekCAString
--- headers/libsuperposition_client.h
+++ headers/libsuperposition_client.h
@@ -24,3 +24,23 @@ char *get_applicable_variant(struct Arc_Client *client, const char *c_context, s
 char *get_satisfied_experiments(struct Arc_Client *client, const char *c_context);
 
 char *get_running_experiments(struct Arc_Client *client);
+
+int last_error_length(void);
+
+const char *last_error_message(void);
+
+void free_string(char *s);
+
+int new_client(const char *tenant, unsigned long update_frequency, const char *hostname);
+
+void start_polling_update(const char *tenant);
+
+void free_client(struct Arc_Client *ptr);
+
+struct Arc_Client *get_client(const char *tenant);
+
+char *get_applicable_variant(struct Arc_Client *client, const char *c_context, short toss);
+
+char *get_satisfied_experiments(struct Arc_Client *client, const char *c_context);
+
+char *get_running_experiments(struct Arc_Client *client);
