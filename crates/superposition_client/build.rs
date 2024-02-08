39bd09040c24480525cd060560de4b4b8aa2288c
Natarajan Kannan <natarajan@juspay.in>
2024-02-08 19:25:04 +0530

    docs:  add intro doc and features
    feat: added HS cac client
    
    fix: a time bug with last_modified
    
    feat: added experimentation client
    
    feat: a working nix build
    
    feat: migrate from naersk to crane
    
    feat: flake parts added
    
    feat: nixified hs-cac-client
    
    feat: completed nixification

---
+++ crates/superposition_client/build.rs
@@ -0,0 +1,10 @@
+use std::env;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let mut config: cbindgen::Config = Default::default();
+    config.language = cbindgen::Language::C;
+    cbindgen::generate_with_config(&crate_dir, config)
+      .unwrap()
+      .write_to_file("../../headers/libsuperposition_client.h");
+}
