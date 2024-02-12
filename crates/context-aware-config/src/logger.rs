660c642a3ce4fcd0051ed41d643c5a0756a90901
Kartik <kartik.gajendra@juspay.in>
2024-02-12 14:39:42 +0530

    fix:  better logging

--- crates/context-aware-config/src/logger.rs
+++ crates/context-aware-config/src/logger.rs
@@ -1,116 +1,42 @@
-use crate::helpers::parse_headermap_safe;
+use actix_http::header::{HeaderMap, HeaderValue};
 use actix_web::{
     body::MessageBody,
-    dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform},
-    web::BytesMut,
-    Error, HttpMessage,
+    dev::{ServiceRequest, ServiceResponse},
+    Error,
 };
-use futures::future::{ready, LocalBoxFuture, Ready};
-use futures::{FutureExt, StreamExt};
-use serde_json::{json, Value};
 use service_utils::helpers::get_pod_info;
-use std::{cell::RefCell, rc::Rc};
-use tracing::{span, Level, Span};
-use tracing_actix_web::{DefaultRootSpanBuilder, RootSpanBuilder};
-use tracing_subscriber::filter::EnvFilter;
-
-//---------------- GoldenSignal Middleware <start> -----------------
-
-// There are two steps in middleware processing.
-// 1. Middleware initialization, middleware factory gets called with
-//    next service in chain as parameter.
-// 2. Middleware's call method gets called with normal request.
-
-pub struct GoldenSignalFactory;
-
-// Middleware factory is `Transform` trait
-// `S` - type of the next service
-// `B` - type of response's body
-impl<S, B> Transform<S, ServiceRequest> for GoldenSignalFactory
-where
-    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
-    S::Future: 'static,
-    B: 'static + std::fmt::Debug,
-{
-    type Response = ServiceResponse<B>;
-    type Error = Error;
-    type InitError = ();
-    type Transform = GoldenSignal<S>;
-    type Future = Ready<Result<Self::Transform, Self::InitError>>;
-
-    fn new_transform(&self, service: S) -> Self::Future {
-        ready(Ok(GoldenSignal {
-            service: Rc::new(RefCell::new(service)),
-        }))
-    }
-}
-
-pub struct GoldenSignal<S> {
-    service: Rc<RefCell<S>>,
-}
-
-impl<S, B> Service<ServiceRequest> for GoldenSignal<S>
-where
-    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
-    S::Future: 'static,
-    B: 'static + std::fmt::Debug,
-{
-    type Response = ServiceResponse<B>;
-    type Error = Error;
-    type Future = LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
-
-    forward_ready!(service);
-
-    fn call(&self, mut req: ServiceRequest) -> Self::Future {
-        let svc = self.service.clone();
-
-        async move {
-            let mut body = BytesMut::new();
-            let mut payload = req.take_payload();
-            while let Some(chunk) = payload.next().await {
-                let chunk = chunk?;
-                body.extend_from_slice(&chunk);
-            }
-
-            let (_payload_sender, mut payload) = actix_http::h1::Payload::create(true);
-            payload.unread_data(body.clone().into());
-            req.set_payload(payload.into());
-
-            let req_headers = parse_headermap_safe(req.headers());
-            let res = svc.call(req).await?;
-            let res_headers = parse_headermap_safe(res.headers());
-            let res_body = res.response().body();
-            let req_body = String::from_utf8(body.freeze().to_vec())
-                .and_then(|s| {
-                    Ok(serde_json::from_str::<serde_json::Value>(s.as_str())
-                        .unwrap_or(Value::Null))
-                })
-                .unwrap_or(Value::Null);
-
-            tracing::info!(
-                request_body = format!("{}", req_body),
-                request_headers = format!("{}", json!(req_headers)),
-                reponse_body = format!("{:?}", res_body),
-                response_headers = format!("{}", json!(res_headers)),
-                http.status_code = res.status().as_u16(),
-                "GoldenSignal",
-            );
-            Ok(res)
-        }
-        .boxed_local()
-    }
-}
-
-//---------------- GoldenSignal Middleware <end> -----------------
+use tracing_utils::tracing_actix_web::{DefaultRootSpanBuilder, RootSpanBuilder};
+use tracing_utils::tracing_subscriber::filter::EnvFilter;
+use tracing_utils::{
+    tracing::{span, Level, Span},
+    tracing_actix_web,
+};
 
 pub struct CustomRootSpanBuilder;
 
 impl RootSpanBuilder for CustomRootSpanBuilder {
     fn on_request_start(request: &ServiceRequest) -> Span {
         let (pod_identifier, deployment_id) = get_pod_info();
+        let headers = request.headers();
+        let extractor_header = |headers: &HeaderMap, key: &str, default: &str| {
+            headers
+                .get(key)
+                .map(HeaderValue::to_str)
+                .unwrap_or(Ok(default))
+                .unwrap_or(default)
+                .to_owned()
+        };
+        let tenant = extractor_header(headers, "x-tenant", "no-tenant-header");
+        let user_agent = extractor_header(headers, "user-agent", "no-user-agent");
+        let method = request.method().to_string();
+        let path = request.path();
         tracing_actix_web::root_span!(
             request,
             service = "context-aware-config",
+            tenant,
+            user_agent,
+            method,
+            path,
             pod_id = pod_identifier,
             deployment_id = deployment_id
         )
@@ -133,10 +59,8 @@ impl RootSpanBuilder for CustomRootSpanBuilder {
     }
 }
 
-//let custom_middleware = TracingLogger::<CustomRootSpanBuilder>::new();
-
 pub fn init_log_subscriber() {
-    let subscriber = tracing_subscriber::fmt::Subscriber::builder()
+    let subscriber = tracing_utils::tracing_subscriber::fmt::Subscriber::builder()
         .with_env_filter(EnvFilter::from_default_env());
     if Ok(String::from("DEV")) == std::env::var("APP_ENV") {
         subscriber.compact().init();
