df72896e42d4a2836cd9c27c751f768fd67607b7
Kartik <kartik.gajendra@juspay.in>
2024-02-26 17:57:48 +0530

    fix:  transpose columns in single experiment page for variants

--- crates/frontend/src/components/experiment/utils.rs
+++ crates/frontend/src/components/experiment/utils.rs
@@ -1,26 +1,39 @@
-use crate::types::{Variant, VariantType};
+use std::collections::HashMap;
+
+use crate::{
+    components::table::types::Column,
+    types::{Variant, VariantType},
+};
+use leptos::{view, IntoView};
 use serde_json::{Map, Value};
 
-pub fn gen_variant_rows(variants: &[Variant]) -> Result<Vec<Map<String, Value>>, String> {
-    let rows = variants
-        .iter()
-        .enumerate()
-        .map(|(i, variant)| {
-            let variant_name = match variant.variant_type {
-                VariantType::CONTROL => "Control".into(),
-                VariantType::EXPERIMENTAL => format!("Variant-{i}"),
-            };
-            let mut row_data = variant
-                .overrides
-                .iter()
-                .map(|(key, value)| (key.clone(), value.clone()))
-                .collect::<Vec<(String, Value)>>();
-            row_data.extend_from_slice(&[
-                (String::from("Variant"), variant_name.into()),
-                (String::from("variant_id"), variant.id.clone().into()),
-            ]);
-            Map::from_iter(row_data)
-        })
-        .collect::<Vec<Map<String, Value>>>();
-    Ok(rows)
+pub fn gen_variant_table(
+    variants: &[Variant],
+) -> Result<(Vec<Map<String, Value>>, Vec<Column>), String> {
+    let mut columns = vec![Column::default("Config Key".into())];
+    let mut row_map: HashMap<&String, Map<String, Value>> = HashMap::new();
+    for (i, variant) in variants.iter().enumerate() {
+        let name = match variant.variant_type {
+            VariantType::CONTROL => format!("{}", variant.variant_type),
+            VariantType::EXPERIMENTAL => format!("Variant-{}", i),
+        };
+        columns.push(Column::new(name.clone(), None, |value: &str, _| {
+            view! { <span>{value.to_string()}</span> }.into_view()
+        }));
+        for (config, value) in variant.overrides.iter() {
+            match row_map.get_mut(config) {
+                Some(c) => {
+                    c.insert(name.clone(), value.clone());
+                }
+                None => {
+                    let mut m = Map::new();
+                    m.insert("Config Key".into(), Value::String(config.clone()));
+                    m.insert(name.clone(), value.clone());
+                    row_map.insert(config, m);
+                }
+            }
+        }
+    }
+    let rows = row_map.into_values().collect();
+    Ok((rows, columns))
 }
