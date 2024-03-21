e673fb15a9337be92c34f30c0700363388d756f5
Shubhranshu Sanjeev <shubhranshu.sanjeev@juspay.in>
2024-03-21 17:26:56 +0530

    feat: [PICAF-25423] added new result, error type and error macros

---
+++ crates/service-utils/src/result.rs
@@ -0,0 +1,113 @@
+use actix_web::{
+    error,
+    http::{header::ContentType, StatusCode},
+    HttpResponse,
+};
+use derive_more::Display;
+use serde_json::json;
+use thiserror::Error as this_error;
+
+#[derive(this_error)]
+pub enum AppError {
+    #[error("validation failed ( `{0}` )")]
+    ValidationError(String),
+    #[error("bad arguments ( `{0}` )")]
+    BadArgument(String),
+    #[error(transparent)]
+    DbError(#[from] diesel::result::Error),
+    #[error(transparent)]
+    ServerError(#[from] ServerError),
+    #[error(transparent)]
+    UnexpectedError(anyhow::Error),
+}
+
+#[derive(Debug, this_error, Display, Clone)]
+#[display(
+    fmt = "server returned an error: {} with status code {}",
+    message,
+    status_code
+)]
+pub struct ServerError {
+    pub message: String,
+    pub status_code: StatusCode,
+}
+
+pub type Result<T> = core::result::Result<T, AppError>;
+
+impl AppError {
+    fn generate_err_response(code: StatusCode, msg: &str) -> HttpResponse {
+        let response = json!({ "message" : msg });
+        HttpResponse::build(code)
+            .insert_header(ContentType::json())
+            .json(response)
+    }
+}
+
+impl error::ResponseError for AppError {
+    fn error_response(&self) -> HttpResponse {
+        use diesel::result::DatabaseErrorKind as diesel_error_kind;
+        use diesel::result::Error as diesel_error;
+
+        log::error!("{}", self);
+        match self {
+            AppError::ValidationError(msg) => {
+                Self::generate_err_response(StatusCode::BAD_REQUEST, msg)
+            }
+            AppError::BadArgument(msg) => {
+                Self::generate_err_response(StatusCode::BAD_REQUEST, msg)
+            }
+            AppError::UnexpectedError(_) => Self::generate_err_response(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Something went wrong",
+            ),
+
+            AppError::DbError(diesel_error::InvalidCString(_)) => {
+                Self::generate_err_response(
+                    StatusCode::SERVICE_UNAVAILABLE,
+                    "Something went wrong",
+                )
+            }
+
+            AppError::DbError(diesel_error::NotFound) => Self::generate_err_response(
+                StatusCode::NOT_FOUND,
+                "No records found. Please refine or correct your search parameters",
+            ),
+
+            AppError::DbError(diesel_error::DatabaseError(
+                diesel_error_kind::UniqueViolation
+                | diesel_error_kind::CheckViolation
+                | diesel_error_kind::NotNullViolation
+                | diesel_error_kind::ForeignKeyViolation,
+                error,
+            )) => Self::generate_err_response(StatusCode::CONFLICT, error.message()),
+
+            AppError::DbError(_) => Self::generate_err_response(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Something went wrong",
+            ),
+
+            AppError::ServerError(error) => {
+                Self::generate_err_response(error.status_code, &error.message)
+            }
+        }
+    }
+}
+
+fn error_chain_fmt(
+    e: &dyn std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
+}
+
+impl std::fmt::Debug for AppError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
