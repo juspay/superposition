b94f7988df5ae89a104d2d76e974efeb73836869
Pratik Mishra <pratik.mishra@juspay.in>
2024-03-28 12:23:26 +0530

    feat: -js-secure-sandbox

--- crates/context-aware-config/tests/cac_tests.rs
+++ crates/context-aware-config/tests/cac_tests.rs
@@ -4,50 +4,37 @@ use serde_json::json;
 // #[test] //todo : currently there is issue in running this test
 fn test_execute_fn() {
     let code_ok = r#"
-        function test_fun() {
+        function validate() {
             return true;
         };
     "#;
 
     let execute_code_error = r#"
-        function test_fun() {
+        function validate() {
             return false;
         }
     "#;
 
     let compile_code_error = r#"
-        function test_fun( {
+        function validate( {
             return true;
         }
     "#;
 
-    let err_execute = match execute_fn(
-        &(execute_code_error.to_owned()),
-        &"test_fun".to_owned(),
-        "test",
-        json!(10),
-    ) {
-        Ok(_) => false,
-        Err((e, stdout)) => e.contains("Bad schema"),
-    };
-    let err_compile =
-        match compile_fn(&(compile_code_error.to_owned()), &"test_fun".to_owned()) {
-            Ok(()) => false,
-            Err(e) => e.contains("Bad schema"),
+    let err_execute =
+        match execute_fn(&(execute_code_error.to_owned()), "test", json!(10)) {
+            Ok(_) => false,
+            Err((e, stdout)) => e.contains("Bad schema"),
         };
+    let err_compile = match compile_fn(&(compile_code_error.to_owned())) {
+        Ok(()) => false,
+        Err(e) => e.contains("Bad schema"),
+    };
     assert_eq!(
-        execute_fn(
-            &(code_ok.to_owned()),
-            &"test_fun".to_owned(),
-            "test",
-            json!(10)
-        ),
+        execute_fn(&(code_ok.to_owned()), "test", json!(10)),
         Ok("true".to_string())
     );
     assert_eq!(err_execute, true);
-    assert_eq!(
-        compile_fn(&(code_ok.to_owned()), &"test_fun".to_owned()),
-        Ok(())
-    );
+    assert_eq!(compile_fn(&(code_ok.to_owned())), Ok(()));
     assert_eq!(err_compile, true);
 }
