8d72d99820caa4c40a1ec2f7d50b3a7e22a509c4
Kartik <kartik.gajendra@juspay.in>
2024-04-17 11:23:48 +0530

    feat: created new types crate for superposition movement

---
+++ crates/superposition_types/src/lib.rs
@@ -0,0 +1,67 @@
+use actix::fut::{ready, Ready};
+use actix_web::{dev::Payload, error, FromRequest, HttpMessage, HttpRequest};
+use log::error;
+use serde_json::json;
+
+pub trait SuperpositionUser {
+    fn get_email(&self) -> String;
+    fn get_username(&self) -> String;
+    fn get_auth_token(&self) -> String;
+}
+
+#[derive(Debug, Clone)]
+pub struct User {
+    email: String,
+    username: String,
+    auth_token: String,
+}
+
+impl SuperpositionUser for User {
+    fn get_email(&self) -> String {
+        self.email.clone()
+    }
+
+    fn get_username(&self) -> String {
+        self.username.clone()
+    }
+
+    fn get_auth_token(&self) -> String {
+        self.auth_token.clone()
+    }
+}
+
+impl Default for User {
+    fn default() -> Self {
+        Self {
+            email: "superposition@juspay.in".into(),
+            username: "superposition".into(),
+            auth_token: "1234abcd".into(),
+        }
+    }
+}
+
+impl From<Box<dyn SuperpositionUser>> for User {
+    fn from(value: Box<dyn SuperpositionUser>) -> Self {
+        User {
+            email: value.get_email(),
+            username: value.get_username(),
+            auth_token: value.get_auth_token(),
+        }
+    }
+}
+
+impl FromRequest for User {
+    type Error = actix_web::error::Error;
+    type Future = Ready<Result<Self, Self::Error>>;
+
+    fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
+        if let Some(user) = req.extensions().get::<User>() {
+            ready(Ok(user.to_owned()))
+        } else {
+            error!("No user was found while validating token");
+            ready(Err(error::ErrorUnauthorized(
+                json!({"message":"invalid token provided"}),
+            )))
+        }
+    }
+}
