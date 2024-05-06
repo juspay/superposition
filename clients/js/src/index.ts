dbd101eff23b547ecb188eb0103a375bd1cb1874
Ayush Jain <76424614+ayushjain17@users.noreply.github.com>
2024-05-06 15:51:22 +0530

    feat: Add JS experiment client (#26)

--- clients/js/src/index.ts
+++ clients/js/src/index.ts
@@ -1,7 +1,7 @@
 import * as jsonLogic from 'json-logic-js';
 import { deepMerge } from './utils/deepMerge';
 import { compareSemanticIsGreater } from './utils/operations'
-import { IObject, Dimension } from './types'
+import { IObject, Dimension, Experiments, Variant, VariantType, Variants } from './types'
 
 type DataFromCacApi = {
     contexts: Array<Dimension>;
@@ -37,5 +37,57 @@ export class CacReader {
 
         const targetConfig: IObject = { ...this.defaultConfig };
         return deepMerge(targetConfig, ...requiredOverrides);
+    }
+}
+
+export class ExperimentReader {
+    experiments: Experiments;
+
+    constructor(experiments: Experiments) {
+        this.experiments = experiments;
+    }
+
+    public getApplicableVariant(data: IObject, toss: number): Array<String> {
+        if (!Number.isInteger(toss)) {
+            throw new Error("Invalid toss, valid range: -1 to 100");
+        }
+        const experiments = this.getSatisfiedExperiments(data);
+        const variants = [];
+        for (const exp of experiments) {
+            const v = this.decideVariant(exp.traffic_percentage, exp.variants, toss);
+            if (v) {
+                variants.push(v.id)
+            }
+        }
+        return variants;
+    };
+
+    getSatisfiedExperiments(data: IObject): Experiments {
+        return this.experiments.filter(exp => jsonLogic.apply(exp.context, data));
+    };
+
+    // decide which variant to return among all applicable experiments
+    decideVariant(
+        traffic: number,
+        applicable_variants: Variants,
+        toss: number,
+    ): Variant | undefined {
+        if (!Number.isInteger(traffic) || !Number.isInteger(toss)) {
+            return undefined;
+        }
+        if (toss < 0) {
+            for (const variant of applicable_variants) {
+                if (variant.variant_type == VariantType.EXPERIMENTAL) {
+                    return variant;
+                }
+            }
+        }
+        const variant_count = applicable_variants.length;
+        const range = traffic * variant_count;
+        if (toss >= range) {
+            return undefined;
+        }
+        const index = Math.floor(toss / traffic);
+        return applicable_variants[index];
     }
 }
