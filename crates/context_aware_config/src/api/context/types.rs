4453f3f3606ed92247bdc56b0519fcd0c9c57597
Sanchith Hegde <sanchith.hegde@juspay.in>
2024-06-07 19:41:47 +0530

    chore(context_aware_config): add unit test to test deserialization

--- crates/context_aware_config/src/api/context/types.rs
+++ crates/context_aware_config/src/api/context/types.rs
@@ -1,12 +1,14 @@
 use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
 
+#[cfg_attr(test, derive(Debug, PartialEq))] // Derive traits only when running tests
 #[derive(Deserialize, Clone)]
 pub struct PutReq {
     pub context: Map<String, Value>,
     pub r#override: Map<String, Value>,
 }
 
+#[cfg_attr(test, derive(Debug, PartialEq))] // Derive traits only when running tests
 #[derive(Deserialize, Clone)]
 pub struct MoveReq {
     pub context: Map<String, Value>,
@@ -30,6 +32,7 @@ pub struct PaginationParams {
     pub size: Option<u32>,
 }
 
+#[cfg_attr(test, derive(Debug, PartialEq))] // Derive traits only when running tests
 #[derive(serde::Deserialize, Clone)]
 pub enum ContextAction {
     PUT(PutReq),
@@ -56,4 +59,47 @@ pub struct PriorityRecomputeResponse {
     pub condition: Value,
     pub old_priority: i32,
     pub new_priority: i32,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn test_deserialize_context_action() {
+        let put_request = json!({
+            "context": {
+                "foo": "bar",
+                "bar": {
+                    "baz": "baz"
+                }
+            },
+            "override": {
+                "foo": "baz"
+            }
+        });
+
+        let action_str = json!({
+            "PUT": put_request
+        })
+        .to_string();
+
+        let mut expected_context = Map::new();
+        expected_context.insert("foo".to_string(), json!("bar"));
+        expected_context.insert("bar".to_string(), json!({ "baz": "baz"}));
+
+        let mut expected_override = Map::new();
+        expected_override.insert("foo".to_string(), json!("baz"));
+
+        let expected_action = ContextAction::PUT(PutReq {
+            context: expected_context,
+            r#override: expected_override,
+        });
+
+        let action_deserialized =
+            serde_json::from_str::<ContextAction>(&action_str).unwrap();
+
+        assert_eq!(action_deserialized, expected_action);
+    }
 }
