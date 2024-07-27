f394d79b22e1d9a8fd34be2ec0e70a1f7c166551
namit goel <53664454+namitgoel@users.noreply.github.com>
2024-07-27 20:02:24 +0530

    feat: adding python cac and experimentation client wrapper

---
+++ clients/python/main.py
@@ -0,0 +1,44 @@
+from expclient import ExperimentationClient
+from cacclient import CacClient 
+import http.server
+import socketserver
+
+try:
+    tenant_name = "dev"
+    polling_frequency = 1
+    cac_host_name = "http://localhost:8080"
+    exp_client = ExperimentationClient(tenant_name, polling_frequency, cac_host_name)
+    cac_client = CacClient(tenant_name, polling_frequency, cac_host_name)
+
+    exp_client.create_new_experimentation_client()
+    cac_client.create_new_cac_client()
+
+    cac_client.start_cac_polling_update()
+    exp_client.start_experimentation_polling_update()
+
+    PORT = 8002
+
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    class MyHandler(http.server.BaseHTTPRequestHandler):
+        def do_GET(self):
+            # Respond with a 200 OK status
+            if self.path == '/testconfig':
+                cacClientResp = cac_client.get_default_config()
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(cacClientResp.encode())
+            elif self.path == '/testexp':
+                expClientResp = exp_client.get_running_experiments()
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(expClientResp.encode())
+
+    with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+        print(f"Serving at port http://localhost:{PORT}")
+        httpd.serve_forever()
+
+except Exception as e:
+    print("Error:", e)
