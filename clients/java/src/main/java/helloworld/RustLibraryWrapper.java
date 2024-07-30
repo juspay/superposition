7bd90a74d93c6cba8ae80091a013cdfc9d252f3e
Ankit Mahato <ankit.mahato@Ankit-Mahato-T1X660R6LJ.local>
2024-07-30 15:50:55 +0530

    feat: java client

---
+++ clients/java/src/main/java/helloworld/RustLibraryWrapper.java
@@ -0,0 +1,127 @@
+package helloworld;
+
+import java.io.IOException;
+
+import jnr.ffi.LibraryLoader;
+import jnr.ffi.Pointer;
+
+public class RustLibraryWrapper {
+
+    public interface RustLib {
+        String cac_last_error_message();
+
+        int cac_new_client(String tenant, long updateFrequency, String hostname);
+
+        void cac_free_client(long ptr);
+
+        long cac_get_client(String tenant);
+
+        Pointer cac_get_config(long clientPtr, String filterQuery, String filterPrefix);
+
+        Pointer cac_get_default_config(long clientPtr, String filterKeys);
+
+        void cac_start_polling_update(String tenant);
+
+        Pointer cac_get_last_modified(long clientPtr);
+
+        Pointer cac_get_resolved_config(long clientPtr, String filterQuery, String filterPrefix, String merge_strategy);
+
+        void cac_free_string(Pointer s);
+    }
+
+    private static RustLib rustLib;
+
+    public RustLibraryWrapper(String libraryPath, String libraryName) {
+        System.setProperty("jnr.ffi.library.path", libraryPath);
+
+        // Load the Rust library
+        RustLibraryWrapper.rustLib = LibraryLoader.create(RustLib.class).load(libraryName);
+    }
+
+    public int wrappedCacNewClient(String tenant, long updateFrequency, String hostname) throws IOException {
+        int result = rustLib.cac_new_client(tenant, updateFrequency, hostname);
+        if (result > 0) {
+            String errorMessage = rustLib.cac_last_error_message();
+            throw new IOException("Failed to create new client: " + errorMessage);
+        }
+        return result;
+    }
+
+    public long wrappedCacGetClient(String tenant) throws IOException {
+        long clientPtr = rustLib.cac_get_client(tenant);
+        if (clientPtr == 0) {
+            String errorMessage = rustLib.cac_last_error_message();
+            throw new IOException("Failed to get CAC client: " + errorMessage);
+        }
+        return clientPtr;
+    }
+
+    public String getConfig(long clientPtr, String filterQuery, String filterPrefix) throws IOException {
+        Pointer result = rustLib.cac_get_config(clientPtr, filterQuery, filterPrefix);
+        if (result == null) {
+            String errorMessage = rustLib.cac_last_error_message();
+            throw new IOException("Failed to get config: " + errorMessage);
+        }
+        String config = getStringAndFree(result);
+        return config;
+    }
+
+    public String getDefaultConfig(long clientPtr, String filterKeys) throws IOException {
+        Pointer result = rustLib.cac_get_default_config(clientPtr, filterKeys);
+        if (result == null) {
+            String errorMessage = rustLib.cac_last_error_message();
+            throw new IOException("Failed to get default config: " + errorMessage);
+        }
+        String config = getStringAndFree(result);
+        return config;
+    }
+
+    public void startPollingUpdate(String tenant) throws IOException {
+        rustLib.cac_start_polling_update(tenant);
+    }
+
+    public String getLastModified(long clientPtr) throws IOException {
+        Pointer result = rustLib.cac_get_last_modified(clientPtr);
+        if (result == null) {
+            String errorMessage = rustLib.cac_last_error_message();
+            throw new IOException("Failed to get last modified: " + errorMessage);
+        }
+        String lastModified = getStringAndFree(result);
+        return lastModified;
+    }
+
+    public String getResolvedConfig(long clientPtr, String filterQuery, String filterPrefix, String mergeStrategy)
+            throws IOException {
+        Pointer result = rustLib.cac_get_resolved_config(clientPtr, filterQuery, filterPrefix, mergeStrategy);
+        if (result == null) {
+            String errorMessage = rustLib.cac_last_error_message();
+            throw new IOException("Failed to get resolved config: " + errorMessage);
+        }
+        String resolvedConfig = getStringAndFree(result);
+        return resolvedConfig;
+    }
+
+    public String wrappedLastError() throws IOException {
+        String errorMessage = rustLib.cac_last_error_message();
+        if (errorMessage != null) {
+            return errorMessage;
+        } else {
+            return "No error";
+        }
+    }
+
+    public void freeString(Pointer ptr) {
+        rustLib.cac_free_string(ptr);
+    }
+
+    public String getStringAndFree(Pointer ptr) {
+        if (ptr == null) {
+            return null;
+        }
+        try {
+            return ptr.getString(0);
+        } finally {
+            freeString(ptr);
+        }
+    }
+}
