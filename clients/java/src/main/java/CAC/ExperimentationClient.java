03a8948e8a85be60e4916d1ca00bfe8a3a9f111d
Ankit Mahato <ankit.mahato@Ankit-Mahato-T1X660R6LJ.local>
2024-08-05 23:20:41 +0530

    feat: Add expt client wrapper

--- clients/java/src/main/java/CAC/ExperimentationClient.java
+++ clients/java/src/main/java/CAC/ExperimentationClient.java
@@ -1,5 +1,28 @@
 package CAC;
 
+import jnr.ffi.Pointer;
+
+
 public class ExperimentationClient {
-    
+    public interface RustLib {
+        int expt_new_client(String tenant, long updateFrequency, String hostName);
+
+        void expt_free_client(long ptr);
+
+        long expt_get_client(String tenant);
+
+        String expt_last_error_message();
+
+        void expt_free_string(Pointer s);
+
+        void expt_start_polling_update(String tenant);
+
+        Pointer expt_get_applicable_variant(long clientPtr, String context, short toss);
+
+        Pointer expt_get_satisfied_experiments(long clientPtr, String context, String filter_prefix);
+
+        Pointer expt_get_filtered_satisfied_experiments(long clientPtr, String context, String filter_prefix);
+
+        Pointer expt_get_running_experiments(long clientPtr);
+    }
 }
