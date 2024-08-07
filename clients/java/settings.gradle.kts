58b53c573babd62954e9396f45d5da9c2f7c6f0b
Ankit Mahato <ankit.mahato@Ankit-Mahato-T1X660R6LJ.local>
2024-08-07 11:57:40 +0530

    fix: add experiment client implementation

--- clients/java/settings.gradle.kts
+++ clients/java/settings.gradle.kts
@@ -10,4 +10,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "ffi"
+rootProject.name = "superposition"
--- clients/java/src/main/java/CAC/Client.java
+++ clients/java/src/main/java/CAC/Client.java
@@ -3,22 +3,27 @@ package CAC;
 import java.io.File;
 import java.io.IOException;
 
+import jnr.ffi.Pointer;
+
 public class Client {
 
-    public static void main(String[] args) {
+    private static void callCacClient() {
         String dylib = "cac_client";
         File currentDir = new File(System.getProperty("user.dir"));
         String libraryPath = currentDir.getParentFile().getParentFile() + "/target/debug";
         String tenant = "dev";
 
+        System.out.println("------------------------------------------");
+
+        System.out.println("CAC Client");
+
         System.out.println("---------------------");
 
-        // Create an instance of the wrapper class
         CacClient wrapper = new CacClient(libraryPath, dylib);
 
         int newClient;
         try {
-            newClient = wrapper.cacNewClient(tenant, 10, "http://localhost:8080");
+            newClient = wrapper.cacNewClient(tenant, 1, "http://localhost:8080");
             System.out.println("New client created successfully. Client ID: " + newClient);
         } catch (IOException e) {
             System.err.println(e.getMessage());
@@ -26,19 +31,25 @@ public class Client {
 
         System.out.println("---------------------");
 
-        try {
-            wrapper.startPollingUpdate(tenant);
-            System.out.println("Started polling update for tenant: " + tenant);
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
+        Thread pollingThread = new Thread(() -> {
+            try {
+                wrapper.startPollingUpdate(tenant);
+            } catch (IOException e) {
+                System.err.println("Error in polling thread: " + e.getMessage());
+            }
+        });
+        pollingThread.setDaemon(true);
+        pollingThread.start();
+        System.out.println("Started polling in a new thread for tenant: " + tenant);
 
         System.out.println("---------------------");
 
-        long clientPtr;
+        Pointer clientPtr;
         try {
             clientPtr = wrapper.getCacClient(tenant);
             System.out.println("Result from getCacClient: " + clientPtr);
+
+            System.out.println("---------------------");
 
             String config;
             try {
@@ -77,12 +88,104 @@ public class Client {
             } catch (IOException e) {
                 System.err.println(e.getMessage());
             }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static void callExperimentationClient() {
+        String dylib = "experimentation_client";
+        File currentDir = new File(System.getProperty("user.dir"));
+        String libraryPath = currentDir.getParentFile().getParentFile() + "/target/debug";
+        String tenant = "dev";
+
+        System.out.println("------------------------------------------");
+
+        System.out.println("Experimentation Client");
+
+        System.out.println("---------------------");
+
+        ExperimentationClient wrapper = new ExperimentationClient(libraryPath, dylib);
+
+        int newClient;
+        try {
+            newClient = wrapper.exptNewClient(tenant, 1, "http://localhost:8080");
+            System.out.println("New Experimentation client created successfully. Client ID: " + newClient);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        System.out.println("---------------------");
+
+        Thread pollingThread = new Thread(() -> {
+            try {
+                wrapper.startPollingUpdate(tenant);
+            } catch (IOException e) {
+                System.err.println("Error in polling thread: " + e.getMessage());
+            }
+        });
+
+        pollingThread.setDaemon(true); // Optional: Set as daemon thread
+        pollingThread.start();
+        System.out.println("Started polling in a new thread for tenant: " + tenant);
+
+        System.out.println("---------------------");
+
+        Pointer clientPtr;
+        try {
+            clientPtr = wrapper.getExptClient(tenant);
+            System.out.println("Result from getExptClient: " + clientPtr);
+
+            System.out.println("---------------------");
+
+            String runningExperiments;
+            try {
+                runningExperiments = wrapper.getRunningExperiments(clientPtr);
+                System.out.println("runningExperiments: " + runningExperiments);
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+
+            System.out.println("---------------------");
+
+            String satisfiedVariants;
+            try {
+                satisfiedVariants = wrapper.getSatisfiedVariants(clientPtr, "{\"os\": \"android\", \"client\": \"1mg\"}", null);
+                System.out.println("Satisfied Variants: " + satisfiedVariants);
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+
+            System.out.println("---------------------");
+
+            String filteredSatisfiedVariants;
+            try {
+                filteredSatisfiedVariants = wrapper.getFilteredSatisfiedVariants(clientPtr, "{\"os\": \"android\", \"client\": \"1mg\"}", "hyperpay");
+                System.out.println("Filtered Satisfied Variants: " + filteredSatisfiedVariants);
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+
+            System.out.println("---------------------");
+
+            String applicableVariants;
+            try {
+                applicableVariants = wrapper.getApplicableVariants(clientPtr, "{\"os\": \"android\", \"client\": \"1mg\"}", (short) 9);
+                System.out.println("Applicable Variants: " + applicableVariants);
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
 
             System.out.println("---------------------");
 
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+        callCacClient();
+        callExperimentationClient();
     }
 }
 
