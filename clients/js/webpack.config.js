cd52ab97bfd638475a087a57ee37a4c0358dd052
ayush.jain@juspay.in <ayush.jain@juspay.in>
2024-09-01 21:12:56 +0530

    fix: Js Client build output

--- clients/js/webpack.config.js
+++ clients/js/webpack.config.js
@@ -1,36 +1,27 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './dist/index.js',
-  output: {
-    path: path.resolve(__dirname),
-    filename: "index.js",
-    library : {
-        name : "Context-Aware-Config",
-        type: "umd"
+    entry: "./dist/index.js",
+    experiments: {
+        outputModule: true,
     },
-    environment: {
-      arrowFunction: false,
-      bigIntLiteral: false,
-      const: true,
-      destructuring: false,
-      dynamicImport: false,
-      forOf: true,
-      module: true
-    }
-  },
-  module: {
-    rules: [
-      { test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          }
-        }
-      },
-    ]
-  }
+    output: {
+        path: path.resolve(__dirname),
+        filename: "index.js",
+        libraryTarget: "module",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+        ],
+    },
 };
-
