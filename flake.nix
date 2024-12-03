2d9851bb2c4fcbfcf343df42c539b6e2208d9191
ShreyBana <51321651+ShreyBana@users.noreply.github.com>
2024-12-03 15:22:41 +0530

    feat: SAML2 auth (#294)
    authored-by: Shrey Bana <shrey.bana@juspay.in>

--- flake.nix
+++ flake.nix
@@ -8,7 +8,8 @@
     rust-flake.url = "github:juspay/rust-flake";
   };
 
-  outputs = inputs:
+  outputs =
+    inputs:
 
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
@@ -24,33 +25,57 @@
         ./nix/om.nix
       ];
 
-      perSystem = { pkgs, self', config, ... }: {
-        formatter = pkgs.nixpkgs-fmt;
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [
-            self'.devShells.rust
-            self'.devShells.haskell
-            config.pre-commit.devShell
-          ];
-          # Add your devShell tools here
-          packages = with pkgs; [
-            docker-compose
-            gnumake
-            # Why do we need this?
-            stdenv.cc
-            awscli2
-            jq
-            nodejs_18
-            nixpkgs-fmt
-            bacon
-            cargo-watch
-            diesel-cli
-            leptosfmt
-            wasm-pack
-            # go client
-            # go
-          ];
+      perSystem =
+        {
+          pkgs,
+          self',
+          config,
+          ...
+        }:
+        {
+          formatter = pkgs.nixpkgs-fmt;
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [
+              self'.devShells.rust
+              # self'.devShells.haskell
+              config.pre-commit.devShell
+            ];
+            # Add your devShell tools here
+            packages = with pkgs; [
+              docker-compose
+              gnumake
+              # Why do we need this?
+              stdenv.cc
+              awscli2
+              jq
+              nodejs_18
+              nixpkgs-fmt
+              bacon
+              cargo-watch
+              diesel-cli
+              leptosfmt
+              wasm-pack
+              llvmPackages.libclang
+              xmlsec
+              libtool
+              # go client
+              # go
+            ];
+            # Set C flags for Rust's bindgen program. Unlike ordinary C
+            # compilation, bindgen does not invoke $CC directly. Instead it
+            # uses LLVM's libclang. To make sure all necessary flags are
+            # included we need to look in a few places.
+            shellHook = with pkgs;  ''
+              export LIBCLANG_PATH="${llvmPackages.libclang.lib}/lib";
+              export BINDGEN_EXTRA_CLANG_ARGS="$(< ${stdenv.cc}/nix-support/libc-crt1-cflags) \
+                    $(< ${stdenv.cc}/nix-support/libc-cflags) \
+                    $(< ${stdenv.cc}/nix-support/cc-cflags) \
+                    $(< ${stdenv.cc}/nix-support/libcxx-cxxflags) \
+                    ${lib.optionalString stdenv.cc.isClang "-idirafter ${stdenv.cc.cc}/lib/clang/${lib.getVersion stdenv.cc.cc}/include"} \
+                    ${lib.optionalString stdenv.cc.isGNU "-isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc} -isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}/${stdenv.hostPlatform.config} -idirafter ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${lib.getVersion stdenv.cc.cc}/include"} \
+                  "
+            '';
+          };
         };
-      };
     };
 }
