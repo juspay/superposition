9f04e9f207e1f510a72de57b96cc68422862658b
sauraww <66776110+sauraww@users.noreply.github.com>
2024-12-17 20:41:58 +0530

    feat: added update api for default config (#310)

--- crates/frontend/src/components/default_config_form/utils.rs
+++ crates/frontend/src/components/default_config_form/utils.rs
@@ -1,10 +1,28 @@
-use super::types::DefaultConfigCreateReq;
+use super::types::{DefaultConfigCreateReq, DefaultConfigUpdateReq};
 use crate::utils::{construct_request_headers, get_host, parse_json_response, request};
 
 pub async fn create_default_config(
-    key: String,
     tenant: String,
     payload: DefaultConfigCreateReq,
+) -> Result<serde_json::Value, String> {
+    let host = get_host();
+    let url = format!("{host}/default-config");
+
+    let response = request(
+        url,
+        reqwest::Method::POST,
+        Some(payload),
+        construct_request_headers(&[("x-tenant", &tenant)])?,
+    )
+    .await?;
+
+    parse_json_response(response).await
+}
+
+pub async fn update_default_config(
+    key: String,
+    tenant: String,
+    payload: DefaultConfigUpdateReq,
 ) -> Result<serde_json::Value, String> {
     let host = get_host();
     let url = format!("{host}/default-config/{key}");
