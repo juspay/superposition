22f99b2aee4f7afe0ead1395764421d28defcf6a
Kartik <kartik.gajendra@juspay.in>
2024-12-18 17:38:29 +0530

    chore: merge cac and experimentation schemas

--- crates/superposition_types/migrations/2024-05-06-133756_type_templates/up.sql
+++ crates/superposition_types/migrations/2024-05-06-133756_type_templates/up.sql
@@ -9,34 +9,39 @@ CREATE TABLE IF NOT EXISTS public.type_templates (
 CREATE INDEX IF NOT EXISTS type_templates_index ON public.type_templates(type_name);
 CREATE INDEX IF NOT EXISTS type_templates_created_at_index ON public.type_templates(created_at);
 CREATE INDEX IF NOT EXISTS type_templates_last_modifed_index ON public.type_templates(last_modified);
-INSERT INTO public.type_templates(type_name, type_schema, created_by, created_at)
+INSERT INTO public.type_templates(type_name, type_schema, created_by, created_at, last_modified)
 VALUES (
         'Number',
         '{"type": "integer"}',
         'user@superposition.io',
-        NOW()
+        '2024-10-18 10:34:00.376562+00',
+        '2024-10-18 10:34:00.376562+00'
     ),
     (
         'Decimal',
         '{"type": "number"}',
         'user@superposition.io',
-        NOW()
+        '2024-10-18 10:35:00.376562+00',
+        '2024-10-18 10:35:00.376562+00'
     ),
     (
         'Boolean',
         '{"type": "boolean"}',
         'user@superposition.io',
-        NOW()
+        '2024-10-18 10:36:00.376562+00',
+        '2024-10-18 10:36:00.376562+00'
     ),
     (
         'Enum',
         '{"type": "string", "enum": ["android", "ios"]}',
         'user@superposition.io',
-        NOW()
+        '2024-10-18 10:37:00.376562+00',
+        '2024-10-18 10:37:00.376562+00'
     ),
     (
         'Pattern',
         '{"type": "string", "pattern": ".*"}',
         'user@superposition.io',
-        NOW()
+        '2024-10-18 10:38:00.376562+00',
+        '2024-10-18 10:38:00.376562+00'
     );
