2f507dfb85dcb9be5400d8bf3654566e2853e402
PRATIK MISHRA <43684300+pratikmishra356@users.noreply.github.com>
2025-01-03 13:17:23 +0530

    fix: add unique position contraint as deferred (#330)

--- crates/context_aware_config/migrations/2024-12-05-172909_dimension_priority_default/down.sql
+++ crates/context_aware_config/migrations/2024-12-05-172909_dimension_priority_default/down.sql
@@ -1,3 +1,5 @@
 -- This file should undo anything in `up.sql`
 ALTER TABLE public.dimensions
 ALTER COLUMN priority DROP DEFAULT;
+
+ALTER table public.dimensions drop constraint if exists dimension_unique_position;
