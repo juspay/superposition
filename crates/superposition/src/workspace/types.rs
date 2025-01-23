2c0eaa1deff8a9da31626099e2c05f7a591ea5bc
Shubhranshu Sanjeev <shubhranshu.sanjeev@gmail.com>
2025-01-23 15:29:19 +0530

    refactor: workspace ui & form (#373)
    * refactor: workspace ui & form
    
    * refactor: moved api types behind  feature flag

--- crates/superposition/src/workspace/types.rs
+++ crates/superposition/src/workspace/types.rs
@@ -1,19 +1,4 @@
 use serde::Deserialize;
-use superposition_types::database::models::WorkspaceStatus;
-
-#[derive(Debug, Deserialize)]
-pub struct CreateWorkspaceRequest {
-    pub workspace_admin_email: String,
-    pub workspace_name: String,
-    pub workspace_status: Option<WorkspaceStatus>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct UpdateWorkspaceRequest {
-    pub workspace_admin_email: String,
-    pub workspace_status: Option<WorkspaceStatus>,
-    pub mandatory_dimensions: Option<Vec<String>>,
-}
 
 #[derive(Deserialize, Debug)]
 pub struct WorkspaceListFilters {
