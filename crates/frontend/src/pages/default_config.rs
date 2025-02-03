b40d7be9440a0083d73afbc1e6539b92a72d6c9e
Kartik <kartik.gajendra@juspay.in>
2025-02-03 18:02:05 +0530

    fix: prefix grouping was not redirecting to the correct URL

--- crates/frontend/src/pages/default_config.rs
+++ crates/frontend/src/pages/default_config.rs
@@ -77,9 +77,12 @@ pub fn default_config() -> impl IntoView {
 
     let folder_click_handler = move |key_name: Option<String>| {
         let tenant = tenant_rws.get().0;
+        let org_id = org_rws.get().0;
         let redirect_url = match key_name {
-            Some(prefix) => format!("admin/{tenant}/default-config?prefix={prefix}"),
-            None => format!("admin/{tenant}/default-config"),
+            Some(prefix) => {
+                format!("admin/{org_id}/{tenant}/default-config?prefix={prefix}")
+            }
+            None => format!("admin/{org_id}/{tenant}/default-config"),
         };
         logging::log!("redirecting to {:?}", redirect_url.clone());
         let navigate = use_navigate();
