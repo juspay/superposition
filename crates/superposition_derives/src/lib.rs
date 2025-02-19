2391ca4de28af73f3b2ac42483abb6a4161ae993
ayush.jain@juspay.in <ayush.jain@juspay.in>
2025-02-19 15:12:18 +0530

    feat: Move experiment API types to superposition_types

--- crates/superposition_derives/src/lib.rs
+++ crates/superposition_derives/src/lib.rs
@@ -46,3 +46,66 @@ pub fn json_to_sql_derive(input: TokenStream) -> TokenStream {
 
     TokenStream::from(expanded)
 }
+
+/// Implements `FromSql` trait for converting `Text` type to the type for `Pg` backend
+///
+#[proc_macro_derive(TextFromSql)]
+pub fn text_from_sql_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+
+    let expanded = quote! {
+        impl diesel::deserialize::FromSql<diesel::sql_types::Text, diesel::pg::Pg> for #name {
+            fn from_sql(bytes: diesel::pg::PgValue<'_>) -> diesel::deserialize::Result<Self> {
+                let text = <String as diesel::deserialize::FromSql<diesel::sql_types::Text, diesel::pg::Pg>>::from_sql(bytes)?;
+                text.try_into().map_err(|e: String| Box::<dyn std::error::Error + Send + Sync>::from(e))
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+/// Implements `ToSql` trait for converting the typed data to `Json` type for `Pg` backend
+///
+#[proc_macro_derive(TextToSql)]
+pub fn text_to_sql_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+
+    let expanded = quote! {
+        impl diesel::serialize::ToSql<diesel::sql_types::Text, diesel::pg::Pg> for #name {
+            fn to_sql<'b>(
+                &'b self,
+                out: &mut diesel::serialize::Output<'b, '_, diesel::pg::Pg>,
+            ) -> diesel::serialize::Result {
+                let text: String = self.into();
+                <String as diesel::serialize::ToSql<
+                    diesel::sql_types::Text,
+                    diesel::pg::Pg,
+                >>::to_sql(&text, &mut out.reborrow())
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+/// Implements `FromSql` trait for converting `Text` type to the type for `Pg` backend without running validations on it
+///
+#[proc_macro_derive(TextFromSqlNoValidation)]
+pub fn text_from_sql_derive_no_validation(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+
+    let expanded = quote! {
+        impl diesel::deserialize::FromSql<diesel::sql_types::Text, diesel::pg::Pg> for #name {
+            fn from_sql(bytes: diesel::pg::PgValue<'_>) -> diesel::deserialize::Result<Self> {
+                let text = <String as diesel::deserialize::FromSql<diesel::sql_types::Text, diesel::pg::Pg>>::from_sql(bytes)?;
+                Ok(<#name as DisableDBValidation>::from_db_unvalidated(text))
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
