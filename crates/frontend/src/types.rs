029bfff6c85896285a7a9a00a3e350ac1a97ec03
ayush.jain@juspay.in <ayush.jain@juspay.in>
2025-02-19 22:01:57 +0530

    feat: Use API types from superposition_types in frontend

--- crates/frontend/src/types.rs
+++ crates/frontend/src/types.rs
@@ -1,25 +1,17 @@
-use std::{fmt::Display, str::FromStr, vec::Vec};
-
-use chrono::{DateTime, Utc};
-use core::fmt;
 use derive_more::{Deref, DerefMut};
 use leptos::{ReadSignal, WriteSignal};
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Map, Value};
+use serde_json::{Map, Value};
 use superposition_types::{
     database::{
         models::cac::{DefaultConfig, TypeTemplate},
-        models::experimentation::{ExperimentStatusType, Variant, VariantType},
+        models::experimentation::{Variant, VariantType},
         types::DimensionWithMandatory,
     },
-    Exp, Overrides, SortBy,
+    Exp, Overrides,
 };
 
-use crate::logic::Conditions;
-use crate::{
-    components::dropdown::utils::DropdownOption,
-    pages::experiment_list::utils::ExperimentSortOn,
-};
+use crate::components::dropdown::utils::DropdownOption;
 
 #[derive(Clone, Debug)]
 pub struct AppRoute {
@@ -31,26 +23,21 @@ pub struct AppRoute {
 
 pub type InputVector = Vec<(ReadSignal<String>, WriteSignal<String>)>;
 
-#[derive(Copy, Clone, Debug, Serialize, Deserialize, strum_macros::Display)]
+#[derive(
+    Copy,
+    Clone,
+    Debug,
+    Serialize,
+    Deserialize,
+    strum_macros::Display,
+    strum_macros::EnumString,
+)]
 #[strum(serialize_all = "lowercase")]
 pub enum AppEnv {
     PROD,
     SANDBOX,
     TEST,
     DEV,
-}
-
-impl FromStr for AppEnv {
-    type Err = String;
-    fn from_str(val: &str) -> Result<Self, Self::Err> {
-        match val {
-            "PROD" => Ok(Self::PROD),
-            "SANDBOX" => Ok(Self::SANDBOX),
-            "DEV" => Ok(Self::DEV),
-            "TEST" => Ok(Self::TEST),
-            _ => Err("invalid app env!!".to_string()),
-        }
-    }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -68,79 +55,9 @@ pub struct FunctionTestResponse {
 
 /*********************** Experimentation Types ****************************************/
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ExperimentResponse {
-    pub id: String,
-    pub created_at: DateTime<Utc>,
-    pub created_by: String,
-    pub last_modified: DateTime<Utc>,
-
-    pub name: String,
-    pub override_keys: Vec<String>,
-    pub status: ExperimentStatusType,
-    pub traffic_percentage: i32,
-
-    pub context: Value,
-    pub variants: Value,
-    pub chosen_variant: Option<String>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Deref, DerefMut, PartialEq)]
-pub struct StatusTypes(pub Vec<ExperimentStatusType>);
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct ExperimentListFilters {
-    pub status: Option<StatusTypes>,
-    pub from_date: Option<DateTime<Utc>>,
-    pub to_date: Option<DateTime<Utc>>,
-    pub experiment_name: Option<String>,
-    pub experiment_ids: Option<String>,
-    pub created_by: Option<String>,
-    pub context: Option<String>,
-    pub sort_on: Option<ExperimentSortOn>,
-    pub sort_by: Option<SortBy>,
-}
-
-impl Display for ExperimentListFilters {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut query_params = vec![];
-        if let Some(status) = &self.status {
-            let status: Vec<String> = status.iter().map(|val| val.to_string()).collect();
-            query_params.push(format!("status={}", status.join(",")));
-        }
-        if let Some(from_date) = self.from_date {
-            query_params.push(format!("from_date={}", from_date));
-        }
-        if let Some(to_date) = self.to_date {
-            query_params.push(format!("to_date={}", to_date));
-        }
-        if let Some(experiment_name) = &self.experiment_name {
-            query_params.push(format!("experiment_name={}", experiment_name));
-        }
-        if let Some(experiment_ids) = &self.experiment_ids {
-            query_params.push(format!("experiment_ids={}", experiment_ids));
-        }
-        if let Some(created_by) = &self.created_by {
-            query_params.push(format!("created_by={}", created_by));
-        }
-        if let Some(context) = &self.context {
-            query_params.push(format!("context={}", context));
-        }
-        if let Some(sort_on) = self.sort_on {
-            query_params.push(format!("sort_on={}", sort_on));
-        }
-        if let Some(sort_by) = &self.sort_by {
-            query_params.push(format!("sort_by={}", sort_by));
-        }
-        write!(f, "{}", query_params.join("&"))
-    }
-}
-
-impl FromIterator<VariantFormT> for Vec<Variant> {
+impl FromIterator<VariantFormT> for Result<Vec<Variant>, String> {
     fn from_iter<T: IntoIterator<Item = VariantFormT>>(iter: T) -> Self {
-        iter.into_iter()
-            .filter_map(|v| Variant::try_from(v).ok())
-            .collect()
+        iter.into_iter().map(Variant::try_from).collect()
     }
 }
 
@@ -180,44 +97,6 @@ pub struct VariantFormTs(Vec<VariantFormT>);
 impl FromIterator<Variant> for VariantFormTs {
     fn from_iter<T: IntoIterator<Item = Variant>>(iter: T) -> Self {
         Self(iter.into_iter().map(VariantFormT::from).collect())
-    }
-}
-
-pub type Variants = Vec<Variant>;
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Experiment {
-    pub(crate) variants: Variants,
-    pub(crate) name: String,
-    pub(crate) id: String,
-    pub(crate) traffic_percentage: u8,
-    pub(crate) context: Conditions,
-    pub(crate) status: ExperimentStatusType,
-    pub(crate) override_keys: Value,
-    pub(crate) created_by: String,
-    pub(crate) created_at: DateTime<Utc>,
-    pub(crate) last_modified: DateTime<Utc>,
-    pub(crate) chosen_variant: Option<String>,
-}
-
-impl From<ExperimentResponse> for Experiment {
-    fn from(value: ExperimentResponse) -> Self {
-        Self {
-            name: value.name,
-            id: value.id,
-            traffic_percentage: value.traffic_percentage as u8,
-            status: value.status,
-            override_keys: json!(value.override_keys),
-            created_by: value.created_by,
-            created_at: value.created_at,
-            last_modified: value.last_modified,
-            chosen_variant: value.chosen_variant,
-            variants: serde_json::from_value(value.variants).unwrap_or_default(),
-            context: Conditions::from_context_json(
-                value.context.as_object().unwrap_or(&Map::new()),
-            )
-            .unwrap_or(Conditions(vec![])),
-        }
     }
 }
 
