1981658bae47f17eacbc221e8371e5dde6dfde2c
Kartik <kartik.gajendra@juspay.in>
2025-04-15 18:29:18 +0530

    test: tests for the org module in ts sdk

--- clients/tests/ts/tests/env.test.ts
+++ clients/tests/ts/tests/env.test.ts
@@ -1,3 +1,6 @@
-export const ENV: any = {
-    baseUrl: "http://127.0.0.1:8080"
+
+export let ENV: any = {
+    baseUrl: "http://127.0.0.1:8080",
+    org_id: "localorg",
+    workspace_id: "test",
 };
