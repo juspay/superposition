863b33fa1c12cbc26bac1e6da59594a090e25b76
ayush.jain@juspay.in <ayush.jain@juspay.in>
2025-05-09 18:51:13 +0530

    fix: Postman test

--- postman/cac/custom types/Get Types/event.test.js
+++ postman/cac/custom types/Get Types/event.test.js
@@ -1,57 +1,64 @@
 /* global pm */
 
-pm.test('expect response be 200', function () {
+pm.test("expect response be 200", function () {
     pm.response.to.be.ok;
     const response = pm.response.json();
-    const modified_response = response.data.map(({ created_at, last_modified_at, last_modified_by, ...rest }) => rest);
+    const modified_response = response.data.map(
+        ({ created_at, last_modified_at, last_modified_by, ...rest }) => rest
+    );
     console.log("The API returned the response", modified_response);
-    pm.expect(JSON.stringify(modified_response)).to.be.eq(JSON.stringify([
-        {
-            "type_name": "Pattern",
-            "type_schema": {
-                "pattern": ".*",
-                "type": "string"
+    pm.expect(JSON.stringify(modified_response)).to.be.eq(
+        JSON.stringify([
+            {
+                type_name: "Pattern",
+                type_schema: {
+                    pattern: ".*",
+                    type: "string",
+                },
+                created_by: "user@superposition.io",
+                description:
+                    "Pattern type is used to represent a string that matches a specific pattern",
+                change_reason: "initial setup",
             },
-            "created_by": "user@superposition.io",
-            "description": "",
-            "change_reason": ""
-        },
-        {
-            "type_name": "Enum",
-            "type_schema": {
-                "enum": ["android", "ios"],
-                "type": "string"
+            {
+                type_name: "Enum",
+                type_schema: {
+                    enum: ["android", "ios"],
+                    type: "string",
+                },
+                created_by: "user@superposition.io",
+                description:
+                    "Enum type is used to represent a fixed set of values",
+                change_reason: "initial setup",
             },
-            "created_by": "user@superposition.io",
-            "description": "",
-            "change_reason": ""
-        },
-        {
-            "type_name": "Boolean",
-            "type_schema": {
-                "type": "boolean"
+            {
+                type_name: "Boolean",
+                type_schema: {
+                    type: "boolean",
+                },
+                created_by: "user@superposition.io",
+                description:
+                    "Boolean type is used to represent true/false values",
+                change_reason: "initial setup",
             },
-            "created_by": "user@superposition.io",
-            "description": "",
-            "change_reason": ""
-        },
-        {
-            "type_name": "Decimal",
-            "type_schema": {
-                "type": "number"
+            {
+                type_name: "Decimal",
+                type_schema: {
+                    type: "number",
+                },
+                created_by: "user@superposition.io",
+                description: "Decimal type is used to represent decimal values",
+                change_reason: "initial setup",
             },
-            "created_by": "user@superposition.io",
-            "description": "",
-            "change_reason": ""
-        },
-        {
-            "type_name": "Number",
-            "type_schema": {
-                "type": "integer"
+            {
+                type_name: "Number",
+                type_schema: {
+                    type: "integer",
+                },
+                created_by: "user@superposition.io",
+                description: "Number type is used to represent numeric values",
+                change_reason: "initial setup",
             },
-            "created_by": "user@superposition.io",
-            "description": "",
-            "change_reason": ""
-        }
-    ]))
+        ])
+    );
 });
