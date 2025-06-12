a48d3bb84a3c933df63eb29c6228ae44598fb7bc
Kartik <kartik.gajendra@juspay.in>
2025-06-12 17:55:29 +0530

    feat: added a CRUD for experiment groups

--- crates/frontend/src/components/delete_modal.rs
+++ crates/frontend/src/components/delete_modal.rs
@@ -1,14 +1,17 @@
 use leptos::*;
 
+use crate::components::change_form::ChangeForm;
+
 #[component]
 pub fn delete_modal(
     modal_visible: ReadSignal<bool>,
-    confirm_delete: Callback<()>,
+    confirm_delete: Callback<String>,
     set_modal_visible: WriteSignal<bool>,
+    #[prop(default = false)] with_change_reason: bool,
     header_text: String,
 ) -> impl IntoView {
     let style = "font-medium rounded-lg text-sm text-center text-white px-5 py-2.5 hover:opacity-75";
-
+    let change_reason_rws = create_rw_signal(String::new());
     view! {
         <Show when=move || modal_visible.get()>
             <div class="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex items-center justify-center z-[99999999]">
@@ -16,10 +19,19 @@ pub fn delete_modal(
                     <div class="modal-box bg-white rounded-lg p-6 shadow-xl border-2 border-lightgray">
                         <h4 class="text-xl font-semibold text-gray-800 mb-4">Confirm Delete</h4>
                         <p class="text-sm text-gray-600 mb-6">{header_text.clone()}</p>
+                        <Show when=move || with_change_reason>
+                            <ChangeForm
+                                title="Change Reason".to_string()
+                                placeholder="Enter a reason for this change".to_string()
+                                class="my-4".to_string()
+                                value=change_reason_rws.get_untracked()
+                                on_change=Callback::new(move |new_reason| change_reason_rws.set(new_reason))
+                            />
+                        </Show>
                         <div class="flex justify-end space-x-4">
                             <button
                                 class=format!("btn bg-purple-500 {style} hover:bg-purple-500")
-                                on:click=move |_| confirm_delete.call(())
+                                on:click=move |_| confirm_delete.call(change_reason_rws.get())
                             >
                                 Yes, Delete
                             </button>
