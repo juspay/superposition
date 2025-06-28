4a53ccbd3c3891ecb7c09346d790ebe73df6b2f0
ayush.jain@juspay.in <ayush.jain@juspay.in>
2025-06-28 20:41:59 +0530

    fix: Sidenav height issue

--- crates/frontend/src/components/side_nav.rs
+++ crates/frontend/src/components/side_nav.rs
@@ -111,19 +111,12 @@ pub fn side_nav(
     });
 
     view! {
-        <div class="max-w-xs z-990 fixed my-4 ml-4 block w-full h-full flex-wrap inset-y-0 items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 shadow-none -translate-x-full transition-transform duration-200 xl:left-0 xl:translate-x-0 xl:bg-transparent">
-            <div class="h-19.5">
-                <A
-                    href="/admin"
-                    class="block px-8 py-6 m-0 text-sm whitespace-nowrap text-slate-700"
-                >
-                    <span class="ml-1 font-semibold transition-all duration-200">
-                        Superposition Platform
-                    </span>
-                </A>
-            </div>
+        <div class="fixed z-990 inset-y-0 xl:left-0 h-full w-full max-w-xs py-4 pl-4 flex flex-col gap-2 overflow-y-auto bg-white xl:bg-transparent rounded-2xl -translate-x-full xl:translate-x-0 transition-transform duration-200">
+            <A href="/admin" class="flex-0 px-8 py-6 text-sm font-semibold text-center text-slate-700 whitespace-nowrap transition-all duration-200">
+                    Superposition Platform
+            </A>
             <Suspense fallback=move || {
-                view! { <Skeleton variant=SkeletonVariant::Content /> }
+                view! { <Skeleton variant=SkeletonVariant::Block style_class="h-10" /> }
             }>
                 <select
                     value=tenant_rws.get().0
@@ -154,10 +147,8 @@ pub fn side_nav(
                         let navigate = use_navigate();
                         navigate(redirect_url.as_str(), Default::default())
                     }
-
-                    class="select w-full max-w-xs shadow-md"
+                    class="flex-0 select w-full max-w-xs shadow-md"
                 >
-
                     {move || {
                         let workspaces = workspace_resource
                             .get()
@@ -185,33 +176,29 @@ pub fn side_nav(
                             }
                         }
                     }}
-
                 </select>
-                // <hr class="h-px mt-0 mb-1 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent"/>
-                <div class="items-center block w-auto max-h-screen overflow-auto h-sidenav grow basis-full">
-                    <ul class="menu">
-                        <For
-                            each=move || app_routes.get()
-                            key=|route: &AppRoute| route.key.to_string()
-                            children=move |route: AppRoute| {
-                                let path = route.path.to_string();
-                                let is_active = location.pathname.get().contains(&path);
-                                view! {
-                                    <li class="mt-1 w-full">
-                                        <NavItem
-                                            href=route.path.to_string()
-                                            icon=route.icon.to_string()
-                                            text=route.label.to_string()
-                                            is_active=is_active
-                                        />
-                                    </li>
-                                }
-                            }
-                        />
-
-                    </ul>
-                </div>
             </Suspense>
+            // <hr class="h-px mt-0 mb-1 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent"/>
+            <ul class="menu flex-1 h-full flex-nowrap gap-1 overflow-auto">
+                <For
+                    each=move || app_routes.get()
+                    key=|route: &AppRoute| route.key.to_string()
+                    children=move |route: AppRoute| {
+                        let path = route.path.to_string();
+                        let is_active = location.pathname.get().contains(&path);
+                        view! {
+                            <li class="w-full">
+                                <NavItem
+                                    href=route.path.to_string()
+                                    icon=route.icon.to_string()
+                                    text=route.label.to_string()
+                                    is_active=is_active
+                                />
+                            </li>
+                        }
+                    }
+                />
+            </ul>
         </div>
     }
 }
