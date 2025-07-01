a85814723d8d692b016cf132684f2f1e51ee958f
Kartik <kartik.gajendra@juspay.in>
2025-07-01 18:06:01 +0530

    chore: update macos-13 builds for x86

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/juspay/superposition/',
+  baseUrl: '/superposition/',
 
   // GitHub pages deployment config.
   organizationName: 'juspay',
