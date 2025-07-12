0e90b25c97ec410af4dee53feb7179cb973a4152
Natarajan Kannan <natarajan@juspay.in>
2025-07-12 05:36:17 +0530

    chore(dist): remove dist folder for testing

--- clients/javascript/open-feature-provider/dist/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function getNestedValue(obj: any, key: string): any;
-export declare function convertToBoolean(value: any, defaultValue: boolean): boolean;
-export declare function convertToString(value: any, defaultValue: string): string;
-export declare function convertToNumber(value: any, defaultValue: number): number;
-export declare function convertToObject<T>(value: any, defaultValue: T): T;
