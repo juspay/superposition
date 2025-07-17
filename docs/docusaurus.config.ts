9b0ad54b78b6eec69fd13876e5d55e75e23909d8
datron <Datron@users.noreply.github.com>
2025-07-17 12:43:07 +0530

    fix: change base URL for docusaurus

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
     },
 
     // Set the production url of your site here
-    url: "https://your-docusaurus-site.example.com",
+    url: "https://juspay.io",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: "/superposition/",
