4a37e598c9ce5b66a576ae52b099bf7fb208b10f
Natarajan Kannan <natarajan@juspay.in>
2025-07-18 21:51:49 +0530

    build: fix gradle/python/npm publish (#604)

--- clients/java/build.gradle.kts
+++ clients/java/build.gradle.kts
@@ -6,13 +6,18 @@ allprojects {
         google()
         gradlePluginPortal()
     }
-    repositories {
-        maven {
-            name = "CodeArtifact"
-            url = uri(System.getenv("CODEARTIFACT_REPOSITORY_ENDPOINT") ?: "")
-            credentials {
-                username = "aws"
-                password = System.getenv("CODEARTIFACT_AUTH_TOKEN")
+
+    apply(plugin = "maven-publish")
+
+    configure<PublishingExtension> {
+        repositories {
+            maven {
+                name = "CodeArtifact"
+                url = uri(System.getenv("CODEARTIFACT_REPOSITORY_ENDPOINT") ?: "https://non.existent.site.here")
+                credentials {
+                    username = "aws"
+                    password = System.getenv("CODEARTIFACT_AUTH_TOKEN")
+                }
             }
         }
     }
