a475cdf830a8d79bb0ea9c3a9d44c3e851465003
Ankit Kumar Mahato <ankit.mahato@juspay.in>
2025-07-30 14:44:53 +0530

    fix: bucketing queries (#628)

--- crates/superposition_types/migrations/2025-06-26-130933_experiment_buckets/down.sql
+++ crates/superposition_types/migrations/2025-06-26-130933_experiment_buckets/down.sql
@@ -1,6 +1,6 @@
 -- This file should undo anything in `up.sql`
-ALTER TABLE public.experiment_groups DROP COLUMN group_type;
+ALTER TABLE public.experiment_groups DROP COLUMN IF EXISTS group_type;
 
-DROP TYPE public.GROUP_TYPE;
+DROP TYPE public.group_type;
 
-ALTER TABLE public.experiment_groups DROP COLUMN buckets;
+ALTER TABLE public.experiment_groups DROP COLUMN IF EXISTS buckets;
