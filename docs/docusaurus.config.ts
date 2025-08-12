139389afd6b1ff733e831018357d21a8c4469c88
Natarajan Kannan <natarajan@juspay.in>
2025-08-12 22:33:01 +0530

    docs: use labels in table

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
     },
 
     // Set the production url of your site here
-    url: "https://d32fun98zecd6d.cloudfront.net",
+    url: "https://juspay.io/",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: "/superposition/",
