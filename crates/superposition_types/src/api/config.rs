98e5438132a0d5992ac0936d75c76c6c8dbddc7c
Natarajan Kannan <natarajan@juspay.in>
2025-09-13 08:15:58 +0530

    feat: remove audit_log from experiments

--- crates/superposition_types/src/api/config.rs
+++ crates/superposition_types/src/api/config.rs
@@ -1,11 +1,11 @@
-use serde::{Deserialize, Serialize};
-use serde_json::{Map, Value};
-use superposition_derives::{IsEmpty, QueryParam};
-
 use crate::{
     custom_query::{CommaSeparatedStringQParams, QueryParam},
     IsEmpty,
 };
+use chrono::{DateTime, Duration, Utc};
+use serde::{Deserialize, Serialize};
+use serde_json::{Map, Value};
+use superposition_derives::{IsEmpty, QueryParam};
 
 #[derive(Deserialize)]
 pub struct ContextPayload {
@@ -26,4 +26,29 @@ pub struct ResolveConfigQuery {
     pub context_id: Option<String>,
     #[query_param(skip_if_empty)]
     pub prefix: Option<CommaSeparatedStringQParams>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct AuditQueryFilters {
+    pub from_date: Option<DateTime<Utc>>,
+    pub to_date: Option<DateTime<Utc>>,
+    pub table: Option<CommaSeparatedStringQParams>,
+    pub action: Option<CommaSeparatedStringQParams>,
+    pub username: Option<String>,
+    pub count: Option<i64>,
+    pub page: Option<i64>,
+}
+
+impl Default for AuditQueryFilters {
+    fn default() -> Self {
+        Self {
+            action: Some("INSERT,UPDATE,DELETE".parse().unwrap()),
+            from_date: Some(Utc::now() - Duration::days(30)),
+            to_date: None,
+            table: None,
+            count: Some(10),
+            page: Some(1),
+            username: None,
+        }
+    }
 }
