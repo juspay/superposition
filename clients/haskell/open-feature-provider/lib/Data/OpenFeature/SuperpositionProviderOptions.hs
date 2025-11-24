27e5272ecfe3e7588cf01d3cff0122de9fd999db
Shrey Bana <shrey.bana@juspay.in>
2025-11-24 16:00:14 +0530

    feat: instances for options

--- clients/haskell/open-feature-provider/lib/Data/OpenFeature/SuperpositionProviderOptions.hs
+++ clients/haskell/open-feature-provider/lib/Data/OpenFeature/SuperpositionProviderOptions.hs
@@ -4,10 +4,20 @@ import Control.Monad.Logger (LogLevel (..))
 import Data.Int (Int64)
 import Data.Text (Text)
 import Network.URI qualified as Net
+import GHC.Generics (Generic)
+import Data.Aeson (ToJSON)
 
 data RefreshOptions = OnDemand Int64 | Poll Int
+  deriving (Generic, Read, Show, ToJSON, Eq)
 
 data Authorization = Bearer Text | Basic Text
+  deriving (Generic, Read, Show, ToJSON, Eq)
+
+-- HACK Orphan instances :(
+deriving instance Generic LogLevel
+deriving instance ToJSON LogLevel
+deriving instance ToJSON Net.URIAuth
+deriving instance ToJSON Net.URI
 
 data SuperpositionProviderOptions = SuperpositionProviderOptions
   { orgId :: Text,
@@ -20,6 +30,7 @@ data SuperpositionProviderOptions = SuperpositionProviderOptions
     experimentationRefreshOptions :: Maybe RefreshOptions,
     logLevel :: LogLevel
   }
+  deriving (Generic, Show, ToJSON, Eq)
 
 defaultProviderOptions :: SuperpositionProviderOptions
 defaultProviderOptions =
