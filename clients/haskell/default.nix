f314a539350db7d3dfc3a6799bf0c29931e32e65
shivaraj-bh <me@shivaraj-bh.in>
2025-12-05 00:06:08 +0530

    chore(clients/haskell): unmarkBroken monad-logger-aeson

--- clients/haskell/default.nix
+++ clients/haskell/default.nix
@@ -24,6 +24,7 @@
           cac_client.custom = _: self'.packages.cac_client;
           experimentation_client.custom = _: self'.packages.experimentation_client;
           superposition_core.custom = _: self'.packages.superposition_core;
+          monad-logger-aeson.broken = false;
         };
       };
       devShells.haskell = pkgs.mkShell {
