fd65be592f6d182f91e5c59a84a415e394209adf
Datron <Datron@users.noreply.github.com>
2026-02-27 12:35:02 +0530

    fix: update build.rs to ignore header file generation in docs.rs (#895)
    * fix: update build.rs to ignore header file generation in docs.rs
    
    * fix: proper cargo comment

--- crates/superposition_core/build.rs
+++ crates/superposition_core/build.rs
@@ -1,8 +1,18 @@
 fn main() {
+    // Skip cbindgen generation when building on docs.rs
+    // Docs.rs has a sandboxed environment that doesn't allow writing to arbitrary paths
+    if std::env::var("DOCS_RS").is_ok() {
+        return;
+    }
+
     let crate_dir = std::env!("CARGO_MANIFEST_DIR");
     let mut config: cbindgen::Config = Default::default();
     config.language = cbindgen::Language::C;
-    cbindgen::generate_with_config(crate_dir, config)
-        .expect("Failed to generate bindings")
-        .write_to_file("../../target/include/superposition_core.h");
+    if let Err(message) =
+        cbindgen::generate_with_config(crate_dir, config).map(|bindings| {
+            bindings.write_to_file("../../target/include/superposition_core.h")
+        })
+    {
+        println!("cargo::error={}", message);
+    }
 }
