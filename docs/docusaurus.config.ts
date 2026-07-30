14acfdcc3d24f8fc3968c7afbc860c1a013c010c
datron <Datron@users.noreply.github.com>
2026-07-30 15:24:56 +0530

    chore: Revert "chore: reroute docs through our domain (#1120)"
    This reverts commit a27d911534a0adf1219e2d5813da25eed92357b0.

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -17,10 +17,10 @@ const config: Config = {
     },
 
     // Set the production url of your site here
-    url: "https://superposition.juspay.io",
+    url: "https://juspay.io/",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/",
+    baseUrl: "/superposition/",
 
     // GitHub pages deployment config.
     organizationName: "juspay",
